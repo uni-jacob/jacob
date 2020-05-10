618b2877bc719bc40a1c6c9e1018116b4df03d35
Daniil <dadyarri@gmail.com>
2020-05-10 11:29:14 +0300

    Add boilerplate for entry point.

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+
+from vkbottle import Bot
+from vkbottle import Message
+
+from keyboard import Keyboards
+
+bot = Bot(os.environ["VK_TOKEN"])
+kbs = Keyboards()
+
+
+@bot.on.message(text="начать", lower=True)
+async def wrapper(ans: Message):
+    await ans("Привет", keyboard=kbs.main_menu())
+
+
+bot.run_polling(skip_updates=False)
