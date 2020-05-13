da01801404473ab38b304ed546ae87fe7f3c56c3
Daniil <dadyarri@gmail.com>
2020-05-13 12:04:08 +0300

    Add rule for state of bot.

--- utils/rules.py
+++ utils/rules.py
@@ -1,6 +1,10 @@
-from vkbottle.rule import AbstractMessageRule
-from vkbottle import Message
 import json
+
+from vkbottle import Message
+from vkbottle.rule import AbstractMessageRule
+
+import utils
+from database.models import State
 
 
 class ButtonRule(AbstractMessageRule):
@@ -8,6 +12,18 @@ class ButtonRule(AbstractMessageRule):
         self.button = button
 
     async def check(self, message: Message) -> bool:
-        payload = json.loads(message.payload)
-        if payload["button"] == self.button:
+        if message.payload is not None:
+            payload = json.loads(message.payload)
+            if payload["button"] == self.button:
+                return True
+
+
+class StateRule(AbstractMessageRule):
+    def __init__(self, state: str):
+        self.state = state
+
+    async def check(self, message: Message) -> bool:
+        user = await utils.get_storage(message.from_id)
+        state = await State.get(description=self.state)
+        if user is not None and user.state_id == state.id:
             return True
