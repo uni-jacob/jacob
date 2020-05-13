dc2d244686442773611b8a83428cbf0026484d72
Daniil <dadyarri@gmail.com>
2020-05-13 13:03:00 +0300

    Add getting first letters of second names of students in selected alma mater and group.

--- database/db.py
+++ database/db.py
@@ -17,3 +17,25 @@ class Database(Base):
             "select * from administrators where vk_id=%s", (user_id,), fetchone=True
         )
         return bool(query)
+
+    def get_unique_second_name_letters(self, user_id: int):
+        """
+        Получает список первых букв фамилий студентов
+        Args:
+            user_id: Идентификатор администратора
+
+        Returns:
+            list: Список первых букв фамилий
+        """
+        data = self.query(
+            "select alma_mater_id, group_id from administrators where vk_id=%s",
+            (user_id,),
+            fetchone=True,
+        )
+        query = self.query(
+            "SELECT DISTINCT substring(second_name from  '^.') FROM students where "
+            "alma_mater_id = %s and group_id=%s ORDER BY substring(second_name from  "
+            "'^.')",
+            (data[0], data[1]),
+        )
+        return [letter for (letter,) in query]
