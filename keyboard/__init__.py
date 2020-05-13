ccbbc108cf9910e7fb7b0bdda9015ceccbfb09c3
Daniil <dadyarri@gmail.com>
2020-05-13 13:04:35 +0300

    Add keyboards with skipping call message, generating alphabet, generating call interface.

--- keyboard/__init__.py
+++ keyboard/__init__.py
@@ -33,3 +33,56 @@ class Keyboards:
             kb.add_button(Text(label="⚙ Настройки", payload={"button": "settings"}))
             kb.add_button(Text(label="🌐 Веб", payload={"button": "web"}))
         return kb.generate()
+
+    @staticmethod
+    def skip_call_message():
+        """
+        Генерирует клавиатуру для пропуска ввода сообщения призыва
+        Returns:
+            JSON-like str: Строка с клавиатурой
+        """
+        kb = Keyboard()
+        kb.add_row()
+        kb.add_button(Text(label="Пропустить", payload={"button": "skip_call_message"}))
+        return kb.generate()
+
+    def alphabet(self, user_id):
+        """
+        Генерирует фрагмент клавиатуры со списком первых букв фамилиий студентов
+
+        Args:
+            user_id: Идентификатор администратора
+
+        Returns:
+            Keyboard: Фрагмент клавиатуры
+        """
+        kb = Keyboard()
+        kb.add_row()
+        alphabet = self.db.get_unique_second_name_letters(user_id)
+        for letter in alphabet:
+            if len(kb.buttons[-1]) == 4:
+                kb.add_row()
+            kb.add_button(
+                Text(label=letter, payload={"button": "letter", "value": letter})
+            )
+        return kb
+
+    def call_interface(self, user_id: int):
+        """
+        Генерирует клавиатуру для выбора призываемых
+
+        Args:
+            user_id: Идентификатор пользователя
+
+        Returns:
+            JSON-like str: Строка с клавиатурой
+        """
+        kb = self.generate_alphabet(user_id)
+        if len(kb.buttons[-1]):
+            kb.add_row()
+        kb.add_button(Text(label="Сохранить", payload={"button": "save_selected"}))
+        kb.add_button(Text(label="Призвать всех", payload={"button": "call_all"}))
+        kb.add_row()
+        kb.add_button(Text(label="Отмена", payload={"button": "cancel_call"}))
+
+        return kb.generate()
