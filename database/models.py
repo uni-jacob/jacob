8943b229169edaa3c536fda0ecec5808ef428c9b
Daniil <dadyarri@gmail.com>
2020-08-04 19:47:16 +0300

    Write models for database.

---
+++ database/models.py
@@ -0,0 +1,274 @@
+from peewee import AutoField
+from peewee import BigIntegerField
+from peewee import BooleanField
+from peewee import CharField
+from peewee import Check
+from peewee import ForeignKeyField
+from peewee import IntegerField
+from peewee import Model
+from peewee import PostgresqlDatabase
+from peewee import TextField
+from peewee import TimestampField
+
+from database.utils import get_db_credentials
+
+db = PostgresqlDatabase(**get_db_credentials())
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class AcademicStatus(BaseModel):
+    id = AutoField(primary_key=True,)
+    description = CharField()
+
+    class Meta:
+        table_name = "academic_statuses"
+
+
+class AlmaMater(BaseModel):
+    id = AutoField(primary_key=True)
+    name = CharField()
+
+    class Meta:
+        table_name = "alma_maters"
+
+
+class Group(BaseModel):
+    id = AutoField(primary_key=True,)
+    group_num = CharField()
+    specialty = TextField()
+    alma_mater = ForeignKeyField(
+        AlmaMater, backref="almamater", on_delete="CASCADE", on_update="CASCADE"
+    )
+
+    class Meta:
+        table_name = "groups"
+
+
+class Administrator(BaseModel):
+    id = AutoField(primary_key=True)
+    group = ForeignKeyField(
+        Group, backref="groups", on_delete="CASCADE", on_update="CASCADE"
+    )
+
+    class Meta:
+        table_name = "administrators"
+
+
+class CachedChat(BaseModel):
+    id = AutoField(primary_key=True)
+    chat_id = IntegerField(unique=True)
+
+    class Meta:
+        table_name = "cached_chats"
+
+
+class ChatType(BaseModel):
+    id = AutoField(primary_key=True,)
+    description = CharField(unique=True)
+
+    class Meta:
+        table_name = "chat_types"
+
+
+class Chat(BaseModel):
+    id = AutoField(primary_key=True,)
+    chat_id = BigIntegerField(unique=True)
+
+    class Meta:
+        table_name = "chats"
+
+
+class FinancialCategory(BaseModel):
+    id = AutoField(primary_key=True,)
+    name = CharField()
+    summ = IntegerField()
+    group = ForeignKeyField(
+        Group, backref="groups", on_delete="CASCADE", on_update="CASCADE"
+    )
+
+    class Meta:
+        table_name = "financial_categories"
+
+
+class Student(BaseModel):
+    id = AutoField(primary_key=True,)
+    vk_id = BigIntegerField(unique=True, constraints=[Check("vk_id > 0")])
+    first_name = CharField()
+    second_name = CharField()
+    group = ForeignKeyField(
+        Group, backref="groups", on_delete="CASCADE", on_update="CASCADE"
+    )
+    academic_status = ForeignKeyField(
+        AcademicStatus,
+        backref="academicstatuses",
+        default=1,
+        on_delete="SET DEFAULT",
+        on_update="CASCADE",
+    )
+
+    class Meta:
+        table_name = "students"
+
+
+class FinancialDonate(BaseModel):
+    id = AutoField(primary_key=True,)
+    category = ForeignKeyField(
+        FinancialCategory,
+        backref="categories",
+        on_delete="CASCADE",
+        on_update="CASCADE",
+    )
+    student = ForeignKeyField(
+        Student, backref="students", on_delete="CASCADE", on_update="CASCADE"
+    )
+    summ = IntegerField()
+    create_date = TimestampField()
+    update_date = TimestampField()
+
+    class Meta:
+        table_name = "financial_donates"
+
+
+class FinancialExpences(BaseModel):
+    id = AutoField(primary_key=True,)
+    category = ForeignKeyField(
+        FinancialCategory,
+        backref="categories",
+        on_delete="CASCADE",
+        on_update="CASCADE",
+    )
+    summ = IntegerField()
+    create_date = TimestampField()
+
+    class Meta:
+        table_name = "financial_expences"
+
+
+class Mailing(BaseModel):
+    id = AutoField(primary_key=True,)
+    name = CharField()
+    group = ForeignKeyField(
+        Group, backref="groups", on_delete="CASCADE", on_update="CASCADE"
+    )
+
+    class Meta:
+        table_name = "mailings"
+
+
+class State(BaseModel):
+    id = AutoField(primary_key=True,)
+    description = CharField()
+
+    class Meta:
+        table_name = "states"
+
+
+class Storage(BaseModel):
+    id = AutoField(primary_key=True,)
+    state_id = ForeignKeyField(
+        State, backref="states", default=1, on_delete="SET DEFAULT", on_update="CASCADE"
+    )
+    current_chat = ForeignKeyField(
+        ChatType,
+        backref="chattypes",
+        default=0,
+        on_delete="SET DEFAULT",
+        on_update="CASCADE",
+    )
+    mailing_id = ForeignKeyField(
+        Mailing,
+        backref="mailings",
+        null=True,
+        default=None,
+        on_delete="SET NULL",
+        on_update="CASCADE",
+    )
+    names_usage = BooleanField(default=False)
+    selected_students = TextField(default="")
+    text = TextField(default="")
+    attaches = TextField(default="")
+
+    class Meta:
+        table_name = "storages"
+
+
+class Subscriptions(BaseModel):
+    id = AutoField(primary_key=True,)
+    student_id = ForeignKeyField(
+        Student, backref="students", on_delete="CASCADE", on_update="CASCADE"
+    )
+    mailing_id = ForeignKeyField(Mailing, backref="mailings")
+    status = BooleanField()
+
+
+if __name__ == "__main__":
+    db.create_tables(
+        [
+            AcademicStatus,
+            Administrator,
+            AlmaMater,
+            CachedChat,
+            ChatType,
+            Chat,
+            Group,
+            FinancialCategory,
+            Student,
+            FinancialDonate,
+            FinancialExpences,
+            Mailing,
+            State,
+            Storage,
+            Subscriptions,
+        ]
+    )
+
+    AcademicStatus.create(id=0, description="Отчислен")
+    AcademicStatus.create(id=1, description="Бюджетная основа")
+    AcademicStatus.create(id=2, description="Контрактная основа")
+    AcademicStatus.create(id=3, description="Целевое обучение")
+    AcademicStatus.create(id=4, description="Иностранный гражданин")
+
+    ChatType.create(id=0, description="Тестовый")
+    ChatType.create(id=1, description="Основной")
+
+    Chat.create(id=1, chat_id=2000000001, group_id=1, chat_type=0, is_active=False)
+    Chat.create(id=2, chat_id=2000000002, group_id=1, chat_type=1, is_active=False)
+
+    AlmaMater.create(id=1, name="ГИАТ (Государственный институт альфа-тестирования)")
+
+    Group.create(
+        id=1,
+        group_num="1",
+        specialty="Альфа-тестирование и разработка",
+        alma_mater_id=1,
+    )
+
+    Student.create(
+        id=1,
+        vk_id=549350532,
+        first_name="Даниил",
+        second_name="Голубев",
+        group_id=1,
+        academic_status=1,
+    )
+
+    Administrator.create(id=1, group_id=1)
+
+    State.create(id=1, description="main")
+    State.create(id=2, description="wait_call_text")
+    State.create(id=3, description="confirm_call")
+
+    Storage.create(
+        id=1,
+        state_id=1,
+        current_chat=0,
+        mailing_id=None,
+        names_usage=False,
+        selected_students="",
+        text="",
+        attaches="",
+    )
