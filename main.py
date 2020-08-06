3a610cc4d1aa333fcd8ecdb46e180d37c518caab
Daniil <dadyarri@gmail.com>
2020-08-06 17:46:00 +0300

    Add greeting just on ["старт", "начать", "start"] keywords.

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import os
 
 from vkwave.bots import SimpleLongPollBot
+from vkwave.bots import TextFilter
 
 bot = SimpleLongPollBot(tokens=os.getenv("VK_TOKEN"), group_id=os.getenv("GROUP_ID"))
 
 
-@bot.message_handler()
+@bot.message_handler(TextFilter(["старт", "начать", "start"]))
 def handle(_) -> str:
-    return f"Hello world!"
+    return f"Привет!"
 
 
 bot.run_forever()
