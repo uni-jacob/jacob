f4fc8d10baa6969d67551ac5c2d30605a36de0bf
Daniil <dadyarri@gmail.com>
2020-08-10 11:44:23 +0300

    :sparkles: Add answering to commands ["старт", "начать", "start"] with main menu keyboard.

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
+import logging
 import os
 
+from vkwave.bots import SimpleBotEvent
 from vkwave.bots import SimpleLongPollBot
 from vkwave.bots import TextFilter
 
+from services import keyboard as kbs
+
+logging.basicConfig(level=logging.DEBUG)
 bot = SimpleLongPollBot(tokens=os.getenv("VK_TOKEN"), group_id=os.getenv("GROUP_ID"))
 
 
 @bot.message_handler(TextFilter(["старт", "начать", "start"]))
-def handle(_) -> str:
-    return "Привет!"
+async def handle(ans: SimpleBotEvent) -> str:
+    await ans.answer(
+        "Привет!", keyboard=kbs.main_menu(ans.object.object.message.peer_id)
+    )
 
 
 bot.run_forever()
