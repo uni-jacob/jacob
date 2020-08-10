34e3d0792f05279ba6365a6d37c8080fe1f7e9ce
Daniil <dadyarri@gmail.com>
2020-08-10 17:02:02 +0300

    :sparkles: Add handler to cancelling call.

--- main.py
+++ main.py
@@ -29,6 +29,17 @@ async def start_call(ans: SimpleBotEvent):
     await ans.answer("Отправьте текст призыва", keyboard=kbs.skip_call_message())
 
 
+@bot.message_handler(filters.ButtonFilter("cancel_call"))
+async def cancel_call(ans: SimpleBotEvent):
+    db.clear_admin_storage(
+        db.get_system_id_of_student(ans.object.object.message.peer_id)
+    )
+    await ans.answer(
+        "Призыв отменён. Возврат на главную.",
+        keyboard=kbs.main_menu(ans.object.object.message.peer_id),
+    )
+
+
 @bot.message_handler(
     filters.StateFilter("wait_call_text") & filters.ButtonFilter("skip_call_message")
 )
