646faf8c4dc572d5a5f011609a9b60ebf9b17cf9
Daniil <dadyarri@gmail.com>
2020-08-10 16:57:39 +0300

    :sparkles: Add keyboards with alphabet and main call interface.

--- services/keyboard.py
+++ services/keyboard.py
@@ -38,3 +38,44 @@ def skip_call_message():
 
     kb.add_text_button(text="🚫 Отмена", payload={"button": "cancel_call"})
     return kb.get_keyboard()
+
+
+def alphabet(user_id):
+    """
+    Генерирует фрагмент клавиатуры со списком первых букв фамилиий студентов
+
+    Args:
+        user_id: Идентификатор администратора
+
+    Returns:
+        Keyboard: Фрагмент клавиатуры
+    """
+    kb = Keyboard()
+    alphabet = utils.get_unique_second_name_letters_in_a_group(user_id)
+    for letter in alphabet:
+        if len(kb.buttons[-1]) == 4:
+            kb.add_row()
+        kb.add_text_button(text=letter, payload={"button": "letter", "value": letter})
+
+    return kb
+
+
+def call_interface(user_id: int):
+    """
+    Генерирует клавиатуру для выбора призываемых
+
+    Args:
+        user_id: Идентификатор пользователя
+
+    Returns:
+        JSON-like str: Строка с клавиатурой
+    """
+    kb = alphabet(user_id)
+    if len(kb.buttons[-1]):
+        kb.add_row()
+    kb.add_text_button(text="✅ Сохранить", payload={"button": "save_selected"})
+    kb.add_text_button(text="👥 Призвать всех", payload={"button": "call_all"})
+    kb.add_row()
+    kb.add_text_button(text="🚫 Отмена", payload={"button": "cancel_call"})
+
+    return kb.get_keyboard()
