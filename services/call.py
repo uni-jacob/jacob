a159a5861cc47191c6d3c4fb66a5800d350c2b91
Daniil <dadyarri@gmail.com>
2020-08-11 19:21:04 +0300

    :sparkles: Add methods for generating call message.

---
+++ services/call.py
@@ -0,0 +1,40 @@
+from database.models import Student
+from database import utils as db
+
+
+def generate_mentions(names_usage: bool, students: str):
+    """
+    Генерирует призыв студентов
+    Args:
+        names_usage: Использование имен студентов
+        students: Список идентификаторов призываемых студентов
+
+    Returns:
+        str: Призыв студентов
+    """
+    mentions = []
+    sep = ", " if names_usage else ""
+    for student in students.split(","):
+        if student:
+            st = Student.get(id=int(student))
+            hint = st["first_name"] if names_usage else "!"
+            mentions.append(f"@id{st.vk_id} ({hint})")
+    return sep.join(mentions)
+
+
+def generate_message(admin_id: int):
+    """
+    Генерирует сообщение для призыва
+    Args:
+        admin_id: Идентификатор пользователя, из хранилища которого провести генерацию
+
+    Returns:
+        str: Сообщение призыва
+    """
+    store = db.get_admin_storage(admin_id)
+    message = store.text or ""
+    students = store.selected_students or ""
+    mentions = generate_mentions(store.names_usage, students)
+    chat_type = "основной" if store.current_chat else "тестовый"
+    message = f"Сообщение будет отправлено в {chat_type} чат:\n{mentions}\n{message}"
+    return message
