4027ba1aeb3c271cf159104b6fea8437300568f5
Daniil <dadyarri@gmail.com>
2020-08-14 19:37:57 +0300

    :sparkles: Add possibility to exit to main menu via button with payload {"button": "main_menu"}.

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from vkwave.bots import TextFilter
 from blueprints import call
 from blueprints import preferences
 from services import keyboard as kbs
+from services.filters import PLFilter
 
 logging.basicConfig(level=logging.DEBUG)
 bot = SimpleLongPollBot(tokens=os.getenv("VK_TOKEN"), group_id=os.getenv("GROUP_ID"))
@@ -15,7 +16,10 @@ bot.dispatcher.add_router(call.call_router)
 bot.dispatcher.add_router(preferences.preferences_router)
 
 
-@bot.message_handler(TextFilter(["старт", "начать", "start", "привет", "hi", "hello"]))
+@bot.message_handler(
+    TextFilter(["старт", "начать", "start", "привет", "hi", "hello"])
+    | PLFilter({"button": "main_menu"})
+)
 async def start(ans: SimpleBotEvent):
     await ans.answer(
         "Привет!", keyboard=kbs.main.main_menu(ans.object.object.message.peer_id)
