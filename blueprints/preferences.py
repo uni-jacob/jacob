3a2868282a74dca59593052daa8638e7c6615735
Daniil <dadyarri@gmail.com>
2020-08-14 18:39:04 +0300

    :speech_balloon: Add parameters of current group to main screen of preferences.

--- blueprints/preferences.py
+++ blueprints/preferences.py
@@ -3,6 +3,7 @@ from vkwave.bots import SimpleBotEvent
 from vkwave.bots import simple_bot_message_handler
 
 from services import filters
+from database import utils as db
 
 preferences_router = DefaultRouter()
 
@@ -11,4 +12,8 @@ preferences_router = DefaultRouter()
     preferences_router, filters.PLFilter({"button": "settings"})
 )
 async def open_preferences(ans: SimpleBotEvent):
-    await ans.answer("тут будут настройки")
+    group_id = db.admin.get_admin_feud(
+        db.students.get_system_id_of_student(ans.object.object.message.peer_id)
+    )
+    group = db.groups.find_group(id=group_id)
+    await ans.answer(f"Настройки группы {group.group_num} ({group.specialty})")
