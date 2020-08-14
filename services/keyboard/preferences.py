950294ff04722238cb0e799d014147a030e54509
Daniil <dadyarri@gmail.com>
2020-08-14 19:40:42 +0300

    :sparkles: Add keyboard with main menu of preferences.

---
+++ services/keyboard/preferences.py
@@ -0,0 +1,17 @@
+from vkwave.bots import Keyboard
+
+JSONStr = str
+
+
+def preferences() -> JSONStr:
+    """
+    Возвращает клавиатуру главного окна настроек
+    Returns:
+        JSONStr: клавиатура
+    """
+    kb = Keyboard()
+    kb.add_text_button("💬 Настроить чаты", payload={"button": "configure_chats"})
+    kb.add_row()
+    kb.add_text_button("👈🏻 Назад", payload={"button": "main_menu"})
+
+    return kb.get_keyboard()
