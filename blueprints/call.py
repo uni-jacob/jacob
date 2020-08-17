ec32d2528f95b61d66c108fd862f589fd0c628a0
Daniil <dadyarri@gmail.com>
2020-08-17 10:18:28 +0300

    :bug: Fix fool defence in call.

--- blueprints/call.py
+++ blueprints/call.py
@@ -118,7 +118,7 @@ async def confirm_call(ans: SimpleBotEvent):
     msg = call.generate_message(admin_id)
     store = db.admin.get_admin_storage(admin_id)
     chat_type = store.current_chat.description.lower()
-    if not msg or store.attaches:
+    if not msg and not store.attaches:
         raise EmptyCallMessage("Сообщение призыва не может быть пустым")
     db.admin.update_admin_storage(
         db.students.get_system_id_of_student(ans.object.object.message.peer_id),
