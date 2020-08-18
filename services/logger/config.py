82ce9dc2e1a359d84376de05ec18d4c5496cd577
Daniil <dadyarri@gmail.com>
2020-08-18 21:29:52 +0300

    :loud_sound: Update loguru config.

--- services/logger/config.py
+++ services/logger/config.py
@@ -1,23 +1,7 @@
-import os
-
-from notifiers.logging import NotificationHandler
-
-fmt = "YY-MM-D@HH:mm:sszz | {module}.{function} | {level} | {message}"
-tg_conf = {"chat_id": os.getenv("TG_CHAT"), "token": os.getenv("TG_TOKEN")}
+fmt = "{time:YY-MM-D@HH:mm:ss zz} | {module}.{function} | {level} | {message}"
 
 config = {
     "handlers": [
-        {
-            "sink": NotificationHandler("telegram", defaults=tg_conf),
-            "format": fmt,
-            "enqueue": True,
-            "level": "ERROR",
-        },
-        {
-            "sink": "jacob_{time}.log",
-            "format": fmt,
-            "rotation": "00:00",
-            "enqueue": True,
-        },
+        {"sink": "jacob.log", "format": fmt, "rotation": "00:00", "enqueue": True,},
     ]
 }
