bb1b21e3b7d10197435f9992b9cf3e2f8f941ea6
Daniil <dadyarri@gmail.com>
2020-08-19 11:56:46 +0300

    :loud_sound: Update loguru config.

--- services/logger/config.py
+++ services/logger/config.py
@@ -1,7 +1,41 @@
-fmt = "{time:YY-MM-D@HH:mm:ss zz} | {module}.{function} | {level} | {message}"
+import os
+
+from services.logger.handlers import TelegramHandler
+
+fmt = "{time:YYYY-MM-DD@HH:mm:ss zz} | {module}.{function} | {level} | {message}"
+extra_fmt = (
+    "{time:YYYY-MM-DD@HH:mm:ss zz} | {module}.{function} | {level} | @id{"
+    "extra[user_id]}: {message}"
+)
+tg_conf = {"token": os.getenv("TG_TOKEN"), "chat": os.getenv("TG_CHAT")}
 
 config = {
     "handlers": [
-        {"sink": "jacob.log", "format": fmt, "rotation": "00:00", "enqueue": True,},
+        {
+            "sink": TelegramHandler(defaults=tg_conf),
+            "format": extra_fmt,
+            "level": "ERROR",
+            "filter": lambda record: "user_id" in record["extra"],
+        },
+        {
+            "sink": TelegramHandler(defaults=tg_conf),
+            "format": fmt,
+            "level": "ERROR",
+            "filter": lambda record: "user_id" not in record["extra"],
+        },
+        {
+            "sink": "jacob.log",
+            "format": extra_fmt,
+            "rotation": "00:00",
+            "enqueue": True,
+            "filter": lambda record: "user_id" in record["extra"],
+        },
+        {
+            "sink": "jacob.log",
+            "format": fmt,
+            "rotation": "00:00",
+            "enqueue": True,
+            "filter": lambda record: "user_id" not in record["extra"],
+        },
     ]
 }
