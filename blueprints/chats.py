13d4f09b858c5975f43384968cc76a331c87e1f0
Daniil <dadyarri@gmail.com>
2020-08-19 15:33:20 +0300

    :sparkles: Add /tr command (fix layout).

--- blueprints/chats.py
+++ blueprints/chats.py
@@ -3,6 +3,7 @@ import os
 from loguru import logger
 from vkwave.api import API
 from vkwave.bots import ChatActionFilter
+from vkwave.bots import CommandsFilter
 from vkwave.bots import DefaultRouter
 from vkwave.bots import SimpleBotEvent
 from vkwave.bots import simple_bot_message_handler
@@ -10,6 +11,7 @@ from vkwave.client import AIOHTTPClient
 
 from database import utils as db
 from services.logger.config import config
+from services.media import translate_string
 
 chats_router = DefaultRouter()
 api_session = API(tokens=os.getenv("VK_TOKEN"), clients=AIOHTTPClient())
@@ -23,3 +25,17 @@ async def greeting(ans: SimpleBotEvent):
     with logger.contextualize(user_id=ans.object.object.message.from_id):
         db.chats.get_or_create_cached_chat(ans.object.object.message.peer_id)
         await ans.answer("Привет!")
+
+
+@simple_bot_message_handler(chats_router, CommandsFilter("tr"))
+@logger.catch()
+async def tr_command(ans: SimpleBotEvent):
+    if msg := ans.object.object.message.reply_message:
+        await ans.answer(translate_string(msg.text),)
+    for msg in ans.object.object.message.fwd_messages:
+        await ans.answer(translate_string(msg.text))
+    if (
+        not ans.object.object.message.reply_message
+        and not ans.object.object.message.fwd_messages
+    ):
+        await ans.answer("Команда используется в ответ на сообщение")
