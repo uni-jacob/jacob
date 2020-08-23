04cbf5b47aa25798a5efcf908c93ebe4afaf021c
Daniil <dadyarri@gmail.com>
2020-08-23 18:57:40 +0300

    :sparkles: Add handler to show menu of financial category.

--- blueprints/finances.py
+++ blueprints/finances.py
@@ -1,5 +1,6 @@
 import os
 
+import hyperjson
 from loguru import logger
 from vkwave.api import API
 from vkwave.bots import DefaultRouter
@@ -7,6 +8,7 @@ from vkwave.bots import SimpleBotEvent
 from vkwave.bots import simple_bot_message_handler
 from vkwave.client import AIOHTTPClient
 
+from database import utils as db
 from services import filters
 from services import keyboard as kbs
 from services.logger.config import config
@@ -22,4 +24,16 @@ async def finances(ans: SimpleBotEvent):
     await ans.answer(
         "Список финансовых категорий",
         keyboard=kbs.finances.list_of_fin_categories(ans.object.object.message.from_id),
+    )
+
+
+@simple_bot_message_handler(
+    finances_router, filters.PLFilter({"button": "fin_category"})
+)
+async def fin_category_menu(ans: SimpleBotEvent):
+    payload = hyperjson.loads(ans.object.object.message.payload)
+    category_object = db.finances.find_fin_category(id=payload["category"])
+    await ans.answer(
+        f'Меню категории "{category_object.name}"',
+        keyboard=kbs.finances.fin_category(payload["category"]),
     )
