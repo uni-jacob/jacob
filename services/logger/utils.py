e8a80c6107570061e59543c57c3ae3926eab98fe
Daniil <dadyarri@gmail.com>
2020-08-24 23:12:52 +0300

    :fire: Remove unused function; Fix imports

--- services/logger/utils.py
+++ services/logger/utils.py
@@ -1,11 +1,4 @@
-import inspect
-
-
-def get_logger_name():
-    return (
-        f"{inspect.getmodulename(inspect.stack()[1].filename)}"
-        f".{inspect.stack()[1].function}"
-    )
+import os
 
 
 def on_production(_):
