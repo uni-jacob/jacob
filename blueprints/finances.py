6640a3d61491784a0cf9b1c83a12919d1a558dc5
Daniil <dadyarri@gmail.com>
2020-08-28 19:02:05 +0300

    :sparkles: Add possibility to add donates.

--- blueprints/finances.py
+++ blueprints/finances.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 import hyperjson
 from loguru import logger
@@ -32,8 +33,103 @@ async def finances(ans: SimpleBotEvent):
 )
 async def fin_category_menu(ans: SimpleBotEvent):
     payload = hyperjson.loads(ans.object.object.message.payload)
-    category_object = db.finances.find_fin_category(id=payload["category"])
-    await ans.answer(
-        f'Меню категории "{category_object.name}"',
-        keyboard=kbs.finances.fin_category(payload["category"]),
+    db.admin.update_admin_storage(
+        db.students.get_system_id_of_student(ans.object.object.message.from_id),
+        category_id=payload.get("category"),
     )
+
+    if payload.get("category"):
+        category_object = db.finances.find_fin_category(id=payload["category"])
+    else:
+        store = db.admin.get_admin_storage(
+            db.students.get_system_id_of_student(ans.object.object.message.from_id)
+        )
+        category_object = db.finances.find_fin_category(id=store.category_id)
+
+    category_name = category_object.name
+
+    await ans.answer(
+        f'Меню категории "{category_name}"', keyboard=kbs.finances.fin_category(),
+    )
+
+
+@simple_bot_message_handler(finances_router, filters.PLFilter({"button": "add_income"}))
+async def add_income(ans: SimpleBotEvent):
+    db.admin.update_admin_storage(
+        db.students.get_system_id_of_student(ans.object.object.message.from_id),
+        state_id=db.bot.get_id_of_state("select_donater"),
+    )
+    await ans.answer(
+        "Выберите студента, сдавшего деньги",
+        keyboard=kbs.finances.fin_list_of_letters(ans.object.object.message.from_id),
+    )
+
+
+@simple_bot_message_handler(
+    finances_router,
+    filters.PLFilter({"button": "half"}) & filters.StateFilter("select_donater"),
+)
+@logger.catch()
+async def select_half(ans: SimpleBotEvent):
+    with logger.contextualize(user_id=ans.object.object.message.from_id):
+        payload = hyperjson.loads(ans.object.object.message.payload)
+        store = db.admin.get_admin_storage(
+            db.students.get_system_id_of_student(ans.object.object.message.from_id)
+        )
+        category = store.category_id
+        await ans.answer(
+            "Выберите студента, сдавшего деньги",
+            keyboard=kbs.call.list_of_letters(payload["half"], "add_income", category),
+        )
+
+
+@simple_bot_message_handler(
+    finances_router,
+    filters.PLFilter({"button": "letter"}) & filters.StateFilter("select_donater"),
+)
+async def select_letter(ans: SimpleBotEvent):
+    with logger.contextualize(user_id=ans.object.object.message.from_id):
+        payload = hyperjson.loads(ans.object.object.message.payload)
+        letter = payload["value"]
+        await ans.answer(
+            f"Список студентов на букву {letter}",
+            keyboard=kbs.call.list_of_students(
+                letter, ans.object.object.message.peer_id, payload.get("letters")
+            ),
+        )
+
+
+@simple_bot_message_handler(
+    finances_router,
+    filters.PLFilter({"button": "student"}) & filters.StateFilter("select_donater"),
+)
+async def select_student(ans: SimpleBotEvent):
+    with logger.contextualize(user_id=ans.object.object.message.from_id):
+        payload = hyperjson.loads(ans.object.object.message.payload)
+        db.admin.update_admin_storage(
+            db.students.get_system_id_of_student(ans.object.object.message.from_id,),
+            selected_students=str(payload["student_id"]),
+            state_id=db.bot.get_id_of_state("enter_donate_sum"),
+        )
+        await ans.answer("Введите сумму дохода", keyboard=kbs.common.empty())
+
+
+@simple_bot_message_handler(
+    finances_router, filters.StateFilter("enter_donate_sum"),
+)
+async def save_donate(ans: SimpleBotEvent):
+    with logger.contextualize(user_id=ans.object.object.message.from_id):
+        text = ans.object.object.message.text
+        if re.match("^\d+$", text):
+            store = db.admin.get_admin_storage(
+                db.students.get_system_id_of_student(ans.object.object.message.from_id)
+            )
+            db.finances.add_or_edit_donate(
+                store.category_id, int(store.selected_students), int(text)
+            )
+            db.admin.clear_admin_storage(
+                db.students.get_system_id_of_student(ans.object.object.message.from_id)
+            )
+            await ans.answer("Доход сохранен", keyboard=kbs.finances.fin_category())
+        else:
+            await ans.answer("Введите только число")
