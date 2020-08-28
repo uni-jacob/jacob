dac28f3513181bb494dcbf918ce04990d9f8898e
Daniil <dadyarri@gmail.com>
2020-08-28 20:07:48 +0300

    :sparkles: Add utility to get debtors.

--- database/utils/finances.py
+++ database/utils/finances.py
@@ -5,6 +5,7 @@ from peewee import fn
 from database import utils as db
 from database.models import FinancialCategory
 from database.models import FinancialDonate
+from database.utils.students import get_active_students
 
 
 def get_list_of_fin_categories(group_id: int) -> t.List[FinancialCategory]:
@@ -64,3 +65,24 @@ def add_or_edit_donate(category_id: int, student_id: int, summ: int):
         return FinancialDonate.create(
             category_id=category_id, student_id=student_id, summ=summ, update_date=None
         )
+
+
+def get_debtors(category_id: int) -> t.List[int]:
+    """
+    Ищет должников (не сдавших деньги на категорию вообще или не всю сумму)
+    Args:
+        category_id: идентификатор категории
+
+    Returns:
+        List[int]: список идентификаторов
+    """
+
+    category = find_fin_category(id=category_id)
+    students = get_active_students(category.group_id)
+    debtors = []
+    for student in students:
+        donate = find_donate(category_id, student.id)
+        if donate is None or donate.summ < category.summ:
+            debtors.append(student.id)
+
+    return debtors
