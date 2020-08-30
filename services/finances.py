10c2f42fdbf202c5f602c1b028b416ef1afe2401
Daniil <dadyarri@gmail.com>
2020-08-30 16:10:00 +0300

    :bug: Add splitting big messages.

--- services/finances.py
+++ services/finances.py
@@ -1,27 +1,34 @@
+import typing as t
+
 from database import utils as db
 
 
-def generate_debtors_call(admin_id: int):
+def generate_debtors_call(admin_id: int) -> t.List[str]:
     """
     Генерирует сообщение призыва должников
     Args:
         admin_id: идентификатор администратора
 
     Returns:
-        str: сообщение призыва должников
+        List[str]: сообщение(я) призыва должников
     """
     store = db.admin.get_admin_storage(admin_id)
     debtors = db.finances.get_debtors(store.category_id)
     category = db.finances.find_fin_category(id=store.category_id)
-    msg_parts = []
+    messages = [""]
     for debtor_id in debtors:
         if donate := db.finances.find_donate(store.category_id, debtor_id):
             summ = category.summ - donate.summ
         else:
             summ = category.summ
         student = db.students.find_student(id=debtor_id)
-        msg_parts.append(
-            f"@id{student.vk_id} ({student.first_name} {student.second_name}) - {summ}"
+        tmp = (
+            f"@id{student.vk_id} ({student.first_name} {student.second_name}) - "
+            f"{summ} руб.\n"
         )
+        if len(messages[-1]) < 2000:
+            messages[-1] += tmp
+        else:
+            messages.append("")
 
-    return ";\n".join(msg_parts)
+    return messages
