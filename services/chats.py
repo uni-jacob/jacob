2b0adf4c9917a4bd95f4d657fd7ecce32459d074
Daniil <dadyarri@gmail.com>
2020-09-15 23:19:38 +0300

    :sparkles: Add function to generate confirm message.

--- services/chats.py
+++ services/chats.py
@@ -1,5 +1,7 @@
+import re
 import typing as t
 
+import requests
 from vkwave.types.objects import MessagesConversationMember
 
 from database.models import Student
@@ -19,11 +21,26 @@ def prepare_set_from_vk(data: t.List[MessagesConversationMember]) -> t.Set[int]:
 
 def prepare_set_from_db(data: t.List[Student]) -> t.Set[int]:
     """
-        Формирует список идентификаторов студентов из базы данных
-        Args:
-            data: Список судентов
+    Формирует список идентификаторов студентов из базы данных
+    Args:
+        data: Список судентов
 
-        Returns:
-            set: идентификаторы ВК студентов
-        """
+    Returns:
+        set: идентификаторы ВК студентов
+    """
     return {student.vk_id for student in data}
+
+
+def get_confirm_message() -> str:
+    """
+    Генерирует подтверждающее сообщение для регистрации чатов
+    Returns:
+        str: подтверждающее сообщение
+    """
+    text = ""
+    query = requests.get("https://fish-text.ru/get", params={"type": "title"})
+    if query.status_code == 200:
+        text = query.json()["text"]
+        text = re.sub('[!@"#№$;%^:&?*()\-_=+{}\[\]|/,<.>]', "", text)
+        text = text.split(" ")[:3]
+    return "-".join(text).lower()
