e9ba95aaf70f67ed4b6e38a92b620b23992e1d35
Daniil <dadyarri@gmail.com>
2020-09-16 20:24:19 +0300

    :fire: :construction: Remove old chat registering system.

--- tests/database/utils/test_chats.py
+++ tests/database/utils/test_chats.py
@@ -4,13 +4,6 @@ from database.utils import chats
 
 
 class TestChats:
-    def test_get_cached_chats(self):
-        from database.models import CachedChat
-
-        test_cached_chat = CachedChat.get(id=1)
-
-        cache = chats.get_cached_chats()
-        it(cache).should.be_equal([test_cached_chat])
 
     def test_is_chat_registered(self):
 
