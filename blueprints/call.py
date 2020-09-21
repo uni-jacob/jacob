71a00d8efd45185b83403eb4670d0e1911591593
Daniil <dadyarri@gmail.com>
2020-09-21 21:42:14 +0300

    :recycle: Refactor.

--- blueprints/call.py
+++ blueprints/call.py
@@ -137,7 +137,11 @@ async def select_letter(ans: SimpleBotEvent):
             await ans.answer(
                 f"Список студентов на букву {letter}",
                 keyboard=kbs.call.list_of_students(
-                    letter, ans.object.object.message.peer_id, payload["letters"]
+                    letter,
+                    db.students.get_system_id_of_student(
+                        ans.object.object.message.peer_id
+                    ),
+                    payload["letters"],
                 ),
             )
         else:
