d9a0ad1480ece9770aebc697aafb776909d96a42
Daniil <dadyarri@gmail.com>
2020-10-01 18:42:32 +0300

    :sparkles: Add checking if group has not any registered chats.

--- blueprints/call.py
+++ blueprints/call.py
@@ -34,14 +34,24 @@ logger.configure(**config)
 @logger.catch()
 async def start_call(ans: SimpleBotEvent):
     with logger.contextualize(user_id=ans.object.object.message.from_id):
-        db.shortcuts.update_admin_storage(
-            db.students.get_system_id_of_student(ans.object.object.message.peer_id),
-            state_id=db.bot.get_id_of_state("wait_call_text"),
-        )
-        await ans.answer(
-            "Отправьте сообщение к призыву. Поддерживаются фотографии и документы",
-            keyboard=kbs.call.skip_call_message(),
-        )
+        if db.chats.get_list_of_chats_by_group(ans.object.object.message.from_id):
+            db.shortcuts.update_admin_storage(
+                db.students.get_system_id_of_student(ans.object.object.message.peer_id),
+                state_id=db.bot.get_id_of_state("wait_call_text"),
+            )
+            await ans.answer(
+                "Отправьте сообщение к призыву. Поддерживаются фотографии и документы",
+                keyboard=kbs.call.skip_call_message(),
+            )
+        else:
+            await ans.answer(
+                "У вашей группы нет зарегистрированных чатов. Возврат в главное меню",
+                keyboard=kbs.main.main_menu(
+                    db.students.get_system_id_of_student(
+                        ans.object.object.message.from_id
+                    )
+                ),
+            )
 
 
 @simple_bot_message_handler(
