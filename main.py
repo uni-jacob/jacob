92d67d29fc11a2adb9914031f1c29a5b817f44f0
Daniil <dadyarri@gmail.com>
2020-10-05 21:26:27 +0300

    :bookmark: Update version to 2.9.0

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from services import keyboard as kbs
 from services.filters import PLFilter
 from services.logger.config import config
 
-__version__ = "2.8.0"
+__version__ = "2.9.0"
 
 from services.logger.handlers import InterceptHandler
 
