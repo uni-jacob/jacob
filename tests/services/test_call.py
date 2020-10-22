5ea26c56b378b9d72671f4eb8d8fa2ddb575ed76
Daniil <dadyarri@gmail.com>
2020-10-22 17:09:04 +0300

    :white_check_mark: Fix some tests in service.call.

--- tests/services/test_call.py
+++ tests/services/test_call.py
@@ -5,18 +5,19 @@ from services import call
 
 class TestCall:
     def test_generate_mentions_with_sep(self):
+
         res = call.generate_mentions(students="2,3", names_usage=True)
 
-        it(res).should.be_equal("@id446545 (fgrgrf), @id465767 (fhfgbv)")
+        it(res).should.be_equal("@id199901833 (Сабина), @id255208457 (Алексей)")
 
     def test_generate_mentions_without_sep(self):
         res = call.generate_mentions(students="2,3", names_usage=False)
 
-        it(res).should.be_equal("@id446545 (!)@id465767 (!)")
+        it(res).should.be_equal("@id199901833 (!)@id255208457 (!)")
 
     def test_generate_message(self):
         test_admin_id = 1
 
         res = call.generate_message(test_admin_id)
 
-        it(res).should.be_equal("@id549350532 (!)@id446545 (!)@id465767 (!)\nTest")
+        it(res).should.be_equal("@id549350532 (!)@id199901833 (!)@id255208457 (!)\nTest")
