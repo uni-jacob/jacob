5b49e2c2d01d0be2e3ccccdaa8e5d9c8cbeeca90
Daniil <dadyarri@gmail.com>
2020-10-23 18:39:59 +0300

    :sparkles: Add some service functions to work with reporting.

--- database/utils/report.py
+++ database/utils/report.py
@@ -1,7 +1,39 @@
+from database import utils as db
 from database.models import Issue
 
 
-def get_last_issue_of_user(vk_id: int):
-    return (
+def get_or_create_last_issue_of_user(vk_id: int):
+    last_issue = (
         Issue.select().order_by(Issue.id.desc()).where(Issue.from_id == vk_id).limit(1)
     )
+
+    if last_issue:
+        return last_issue[0]
+    return Issue.create(from_id=vk_id)
+
+
+def update_issue(issue_id: int, **kwargs):
+    """
+    Обновляет ишью в базе данных.
+
+    Args:
+        issue_id: Идентификатор ишью
+        **kwargs: Данные для обновления
+
+    Returns:
+        Issue: Объект ишью
+    """
+    Issue.update(**kwargs).where(Issue.id == issue_id).execute()
+    return Issue.get_by_id(issue_id)
+
+
+def generate_issue_text(vk_id: int):
+    store = db.admin.get_admin_storage(
+        db.students.get_system_id_of_student(vk_id),
+    ).json()
+    is_admin = db.admin.is_user_admin(
+        db.students.get_system_id_of_student(vk_id),
+    )
+    text = db.report.get_or_create_last_issue_of_user(vk_id).text
+
+    return f"{text}\n\n{is_admin=}\n{store=}"
