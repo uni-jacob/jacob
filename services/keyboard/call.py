8c792bcb542d6168376651d2a920713e2c02021c
Daniil <dadyarri@gmail.com>
2020-10-25 15:30:05 +0300

    :fire: Delete old call keyboard.

--- services/keyboard/call.py
+++ services/keyboard/call.py
@@ -121,50 +121,6 @@ def list_of_letters(
     return kb.get_keyboard()
 
 
-def list_of_students(
-    letter: str,
-    admin_id: int,
-    letters: t.Optional[t.List[str]] = None,
-) -> JSONStr:
-    """
-    Генерирует клавиатуру со списком студентов, фамилии которых начинаются на letter.
-
-    Args:
-        letter: Первая буква фамилий
-        admin_id: Идентификатор пользователя
-        letters: Список букв (передается когда существует подменю из диапазонов букв,
-            используется для возврата назад)
-
-    Returns:
-        JSONStr: Строка с клавиатурой
-    """
-    data = db.students.get_list_of_students_by_letter(admin_id, letter)
-    selected = db.shortcuts.get_list_of_calling_students(admin_id)
-    kb = Keyboard()
-    for item in data:
-        if len(kb.buttons[-1]) == 2:
-            kb.add_row()
-        label = " "
-        if item.id in selected:
-            label = "✅ "
-        kb.add_text_button(
-            text=f"{label}{item.second_name} {item.first_name}",
-            payload={
-                "button": "student",
-                "student_id": item.id,
-                "letter": letter,
-                "name": f"{item.second_name} {item.first_name}",
-            },
-        )
-    if kb.buttons[-1]:
-        kb.add_row()
-    if letters:
-        kb.add_text_button(text="◀️ Назад", payload={"button": "half", "half": letters})
-    else:
-        kb.add_text_button(text="◀️ Назад", payload={"button": "skip_call_message"})
-    return kb.get_keyboard()
-
-
 def call_prompt(admin_id: int) -> JSONStr:
     """
     Генерирует клавиатуру с настройкой призыва.
