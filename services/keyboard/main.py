c569ddb817a03c7ea9d54ffe7a3fae52fe49494f
Daniil <dadyarri@gmail.com>
2020-10-29 17:07:29 +0300

    :speech_balloons: Little changes to main menu keyboard layout.

--- services/keyboard/main.py
+++ services/keyboard/main.py
@@ -26,11 +26,11 @@ def main_menu(admin_id: int) -> JSONStr:
         kb.add_row()
     kb.add_text_button(text="📅 Расписание", payload={"button": "schedule"})
     if is_admin:
+        kb.add_text_button(text="📕 Контакты", payload={"button": "contacts"})
         kb.add_row()
         kb.add_text_button(text="⚙ Настройки", payload={"button": "settings"})
         kb.add_text_button(text="🌐 Веб", payload={"button": "web"})
         kb.add_row()
-        kb.add_text_button(text="📕 Контакты", payload={"button": "contacts"})
         kb.add_text_button(
             text="⚠ Сообщить об ошибке",
             payload={"button": "report_error"},
