aaf87428ea8617a79196365ccc7b3048d09e1d09
Daniil <dadyarri@gmail.com>
2020-12-07 13:33:27 +0300

    :construction: [WIP] Refactor code.

--- jacob/blueprints/contacts.py
+++ jacob/blueprints/contacts.py
@@ -2,32 +2,25 @@ import os
 
 import hyperjson
 from loguru import logger
-from vkwave.api import API
-from vkwave.bots import DefaultRouter
-from vkwave.bots import MessageFromConversationTypeFilter
-from vkwave.bots import SimpleBotEvent
-from vkwave.bots import simple_bot_message_handler
-from vkwave.client import AIOHTTPClient
+from vkvawe import bots
 
 from database import utils as db
-from database.models import Student
+from database import models
 from services import filters
 from services import keyboard as kbs
-from services.logger.config import config
+from services.logger import config as logger_config
 
-contacts_router = DefaultRouter()
-api_session = API(tokens=os.getenv("VK_TOKEN"), clients=AIOHTTPClient())
-api = api_session.get_context()
-logger.configure(**config)
+contacts_router = bots.DefaultRouter()
+logger.configure(**logger_config)
 
 
-@simple_bot_message_handler(
+@bots.simple_bot_message_handler(
     contacts_router,
     filters.PLFilter({"button": "contacts"}),
-    MessageFromConversationTypeFilter("from_pm"),
+    bots.MessageFromConversationTypeFilter("from_pm"),
 )
 @logger.catch()
-async def start_contacts(ans: SimpleBotEvent):
+async def _start_contacts(ans: bots.SimpleBotEvent):
     with logger.contextualize(user_id=ans.object.object.message.from_id):
         db.shortcuts.update_admin_storage(
             db.students.get_system_id_of_student(ans.object.object.message.from_id),
@@ -45,13 +38,13 @@ async def start_contacts(ans: SimpleBotEvent):
         )
 
 
-@simple_bot_message_handler(
+@bots.simple_bot_message_handler(
     contacts_router,
     filters.PLFilter({"button": "half"}) & filters.StateFilter("select_student"),
-    MessageFromConversationTypeFilter("from_pm"),
+    bots.MessageFromConversationTypeFilter("from_pm"),
 )
 @logger.catch()
-async def select_half(ans: SimpleBotEvent):
+async def _select_half(ans: bots.SimpleBotEvent):
     with logger.contextualize(user_id=ans.object.object.message.from_id):
         payload = hyperjson.loads(ans.object.object.message.payload)
         await ans.answer(
@@ -68,12 +61,12 @@ async def select_half(ans: SimpleBotEvent):
         )
 
 
-@simple_bot_message_handler(
+@bots.simple_bot_message_handler(
     contacts_router,
     filters.PLFilter({"button": "letter"}) & filters.StateFilter("select_student"),
-    MessageFromConversationTypeFilter("from_pm"),
+    bots.MessageFromConversationTypeFilter("from_pm"),
 )
-async def select_letter(ans: SimpleBotEvent):
+async def _select_letter(ans: bots.SimpleBotEvent):
     with logger.contextualize(user_id=ans.object.object.message.from_id):
         payload = hyperjson.loads(ans.object.object.message.payload)
         letter = payload.get("value")
@@ -87,12 +80,12 @@ async def select_letter(ans: SimpleBotEvent):
         )
 
 
-@simple_bot_message_handler(
+@bots.simple_bot_message_handler(
     contacts_router,
     filters.PLFilter({"button": "student"}) & filters.StateFilter("select_student"),
-    MessageFromConversationTypeFilter("from_pm"),
+    bots.MessageFromConversationTypeFilter("from_pm"),
 )
-async def select_student(ans: SimpleBotEvent):
+async def _select_student(ans: bots.SimpleBotEvent):
     with logger.contextualize(user_id=ans.object.object.message.from_id):
         payload = hyperjson.loads(ans.object.object.message.payload)
         db.shortcuts.update_admin_storage(
@@ -101,7 +94,7 @@ async def select_student(ans: SimpleBotEvent):
             ),
             state_id=db.bot.get_id_of_state("main"),
         )
-        student = Student.get_by_id(payload.get("student_id"))
+        student = models.Student.get_by_id(payload.get("student_id"))
         contacts = f"""
         Контакты {student.first_name} {student.second_name}:
         ВК: @id{student.vk_id}
