37adecc840c39edbc52d3ee37b136647ed03e7ed
Daniil <dadyarri@gmail.com>
2020-12-09 16:44:10 +0300

    :art: :recycle: Refactor code.

--- jacob/database/models.py
+++ jacob/database/models.py
@@ -3,7 +3,7 @@
 import os
 from datetime import datetime
 
-from pony.orm import Database, PrimaryKey, Optional, Required, Set
+from pony.orm import Database, Optional, Required, Set
 
 from jacob.services.db import get_db_credentials
 
@@ -11,46 +11,52 @@ db = Database()
 
 
 class AcademicStatus(db.Entity):
-    id = PrimaryKey(int, auto=True)
-    description = Optional(str, 255, unique=True)
+    """Формы обучения студентов."""
+
+    description = Optional(str, unique=True)
     students = Set("Student")
 
 
 class AdminConfig(db.Entity):
-    id = PrimaryKey(int, auto=True)
-    administrator = Required("Administrator")
+    """Хранилище конфигов админов."""
+
+    admin = Required("Admin")
     names_usage = Optional(bool, default=True)
     active_group = Optional("Group")
     active_chat = Optional("Chat")
 
 
 class AlmaMater(db.Entity):
-    id = PrimaryKey(int, auto=True)
+    """Университеты."""
+
     name = Required(str)
     groups = Set("Group")
 
 
 class Group(db.Entity):
-    id = PrimaryKey(int, auto=True)
-    group_num = Required(str, 255)
+    """Студенческие группы."""
+
+    group_num = Required(str)
     specialty = Required(str)
     alma_mater = Required(AlmaMater)
     admin_configs = Set(AdminConfig)
     chats = Set("Chat")
-    administrators = Set("Administrator")
+    admins = Set("Admin")
     students = Set("Student")
     financial_categories = Set("FinancialCategory")
 
 
 class Chat(db.Entity):
-    id = PrimaryKey(int, auto=True)
+    """Зарегистрированные чаты."""
+
     vk_id = Required(int)
     group = Required(Group)
     admin_configs = Set(AdminConfig)
 
 
-class Administrator(db.Entity):
-    id = PrimaryKey(int, auto=True)
+class Admin(db.Entity):
+    """Админы бота."""
+
     student = Required("Student")
     groups = Set(Group)
     admin_config = Optional(AdminConfig)
@@ -61,7 +67,8 @@ class Administrator(db.Entity):
 
 
 class Student(db.Entity):
-    id = PrimaryKey(int, auto=True)
+    """Студенты."""
+
     vk_id = Required(int)
     first_name = Required(str)
     last_name = Required(str)
@@ -72,22 +79,25 @@ class Student(db.Entity):
     academic_status = Required(AcademicStatus)
     financial_incomes = Set("FinancialIncome")
     issues = Set("Issue")
-    administrators = Set(Administrator)
+    admins = Set(Admin)
 
 
-class CallStorage(db.Entity):
-    id = PrimaryKey(int, auto=True)
-    administrator = Required(Administrator)
+class MentionStorage(db.Entity):
+    """Хранилище призыва."""
+
+    admin = Required(Admin)
 
 
 class ChatRegistrarConfig(db.Entity):
-    id = PrimaryKey(int, auto=True)
-    administrator = Required(Administrator)
+    """Хранилище регистратора чатов."""
+
+    admin = Required(Admin)
     phrase = Optional(str, default="")
 
 
 class FinancialCategory(db.Entity):
-    id = PrimaryKey(int, auto=True)
+    """Финансовые категории."""
+
     name = Optional(str)
     summ = Optional(int)
     group = Required(Group)
@@ -97,13 +107,15 @@ class FinancialCategory(db.Entity):
 
 
 class FinancialConfig(db.Entity):
-    id = PrimaryKey(int, auto=True)
-    administrator = Required(Administrator)
+    """Временное хранилище активной финансовой категории."""
+
+    admin = Required(Admin)
     financial_category = Required(FinancialCategory)
 
 
 class FinancialIncome(db.Entity):
-    id = PrimaryKey(int, auto=True)
+    """Доходы."""
+
     financial_category = Required(FinancialCategory)
     student = Required(Student)
     summ = Required(int)
@@ -112,28 +124,32 @@ class FinancialIncome(db.Entity):
 
 
 class FinancialExpense(db.Entity):
-    id = PrimaryKey(int, auto=True)
+    """Расходы."""
+
     financial_category = Required(FinancialCategory)
     summ = Required(int)
     create_date = Optional(datetime, default=lambda: datetime.now())
 
 
 class Issue(db.Entity):
-    id = PrimaryKey(int, auto=True)
+    """Баги."""
+
     author = Required(Student)
     title = Required(str)
     text = Optional(str)
 
 
 class State(db.Entity):
-    id = PrimaryKey(int, auto=True)
-    description = Optional(str, 255, unique=True)
+    """Статусы боты."""
+
+    description = Optional(str, unique=True)
     state_storages = Set("StateStorage")
 
 
 class StateStorage(db.Entity):
-    id = PrimaryKey(int, auto=True)
-    administrator = Required(Administrator)
+    """Связка состояний ботов с администраторами."""
+
+    admin = Required(Admin)
     state = Required(State)
 
 
