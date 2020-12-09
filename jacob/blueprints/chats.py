80c57a2a6c5effb8a9657b26a0934139095e4f69
Daniil <dadyarri@gmail.com>
2020-12-09 12:57:14 +0300

    :art: :recycle: Refactor code.

--- jacob/blueprints/chats.py
+++ jacob/blueprints/chats.py
@@ -3,11 +3,12 @@
 from loguru import logger
 from vkwave import bots
 
-from services.logger.config import config
+from services import decorators
+from services.logger import config as logger_config
 from services.media import translate_string
 
 chats_router = bots.DefaultRouter()
-logger.configure(**config)
+logger.configure(**logger_config.config)
 
 
 @bots.simple_bot_message_handler(
@@ -15,9 +16,9 @@ logger.configure(**config)
     bots.ChatActionFilter("chat_invite_user"),
 )
 @logger.catch()
+@decorators.context_logger
 async def _greeting(ans: bots.SimpleBotEvent):
-    with logger.contextualize(user_id=ans.object.object.message.from_id):
-        await ans.answer("Привет!")
+    await ans.answer("Привет!")
 
 
 @bots.simple_bot_message_handler(
@@ -26,15 +27,15 @@ async def _greeting(ans: bots.SimpleBotEvent):
     bots.MessageFromConversationTypeFilter("from_chat"),
 )
 @logger.catch()
+@decorators.context_logger
 async def _tr_command(ans: bots.SimpleBotEvent):
-    with logger.contextualize(user_id=ans.object.object.message.from_id):
-        message = ans.object.object.message
-        rep_msg = message.reply_message
-        if rep_msg is not None:
-            await ans.answer(
-                translate_string(rep_msg.text),
-            )
-        for fwd_msg in message.fwd_messages:
-            await ans.answer(translate_string(fwd_msg.text))
-        if not message.reply_message and not message.fwd_messages:
-            await ans.answer("Команда используется в ответ на сообщение")
+    message = ans.object.object.message
+    rep_msg = message.reply_message
+    if rep_msg is not None:
+        await ans.answer(
+            translate_string(rep_msg.text),
+        )
+    for fwd_msg in message.fwd_messages:
+        await ans.answer(translate_string(fwd_msg.text))
+    if not message.reply_message and not message.fwd_messages:
+        await ans.answer("Команда используется в ответ на сообщение")
--- jacob/blueprints/contacts.py
+++ jacob/blueprints/contacts.py
@@ -6,12 +6,12 @@ from vkwave import bots
 
 from database import models
 from database import utils as db
-from services import filters
+from services import decorators, filters
 from services import keyboard as kbs
 from services.logger import config as logger_config
 
 contacts_router = bots.DefaultRouter()
-logger.configure(**logger_config)
+logger.configure(**logger_config.config)
 
 
 @bots.simple_bot_message_handler(
@@ -20,22 +20,22 @@ logger.configure(**logger_config)
     bots.MessageFromConversationTypeFilter("from_pm"),
 )
 @logger.catch()
+@decorators.context_logger
 async def _start_contacts(ans: bots.SimpleBotEvent):
-    with logger.contextualize(user_id=ans.object.object.message.from_id):
-        db.shortcuts.update_admin_storage(
-            db.students.get_system_id_of_student(ans.object.object.message.from_id),
-            state_id=db.bot.get_id_of_state("select_student"),
+    db.shortcuts.update_admin_storage(
+        db.students.get_system_id_of_student(ans.object.object.message.from_id),
+        state_id=db.bot.get_id_of_state("select_student"),
+    )
+    await ans.answer(
+        "Выберите студента для отображения его контактной информации",
+        keyboard=kbs.contacts.ContactsNavigator(
+            db.students.get_system_id_of_student(
+                ans.object.object.message.from_id,
+            ),
         )
-        await ans.answer(
-            "Выберите студента для отображения его контактной информации",
-            keyboard=kbs.contacts.ContactsNavigator(
-                db.students.get_system_id_of_student(
-                    ans.object.object.message.from_id,
-                ),
-            )
-            .render()
-            .menu(),
-        )
+        .render()
+        .menu(),
+    )
 
 
 @bots.simple_bot_message_handler(
@@ -44,21 +44,21 @@ async def _start_contacts(ans: bots.SimpleBotEvent):
     bots.MessageFromConversationTypeFilter("from_pm"),
 )
 @logger.catch()
+@decorators.context_logger
 async def _select_half(ans: bots.SimpleBotEvent):
-    with logger.contextualize(user_id=ans.object.object.message.from_id):
-        payload = ujson.loads(ans.object.object.message.payload)
-        await ans.answer(
-            "Выберите студента",
-            keyboard=kbs.contacts.ContactsNavigator(
-                db.students.get_system_id_of_student(
-                    ans.object.object.message.from_id,
-                ),
-            )
-            .render()
-            .submenu(
-                payload.get("half"),
+    payload = ujson.loads(ans.object.object.message.payload)
+    await ans.answer(
+        "Выберите студента",
+        keyboard=kbs.contacts.ContactsNavigator(
+            db.students.get_system_id_of_student(
+                ans.object.object.message.from_id,
             ),
         )
+        .render()
+        .submenu(
+            payload.get("half"),
+        ),
+    )
 
 
 @bots.simple_bot_message_handler(
@@ -66,18 +66,19 @@ async def _select_half(ans: bots.SimpleBotEvent):
     filters.PLFilter({"button": "letter"}) & filters.StateFilter("select_student"),
     bots.MessageFromConversationTypeFilter("from_pm"),
 )
+@logger.catch()
+@decorators.context_logger
 async def _select_letter(ans: bots.SimpleBotEvent):
-    with logger.contextualize(user_id=ans.object.object.message.from_id):
-        payload = ujson.loads(ans.object.object.message.payload)
-        letter = payload.get("value")
-        await ans.answer(
-            "Список студентов на букву {0}".format(letter),
-            keyboard=kbs.contacts.ContactsNavigator(
-                db.students.get_system_id_of_student(ans.object.object.message.peer_id),
-            )
-            .render()
-            .students(letter),
+    payload = ujson.loads(ans.object.object.message.payload)
+    letter = payload.get("value")
+    await ans.answer(
+        "Список студентов на букву {0}".format(letter),
+        keyboard=kbs.contacts.ContactsNavigator(
+            db.students.get_system_id_of_student(ans.object.object.message.peer_id),
         )
+        .render()
+        .students(letter),
+    )
 
 
 @bots.simple_bot_message_handler(
@@ -85,28 +86,29 @@ async def _select_letter(ans: bots.SimpleBotEvent):
     filters.PLFilter({"button": "student"}) & filters.StateFilter("select_student"),
     bots.MessageFromConversationTypeFilter("from_pm"),
 )
+@logger.catch
+@decorators.context_logger
 async def _select_student(ans: bots.SimpleBotEvent):
-    with logger.contextualize(user_id=ans.object.object.message.from_id):
-        payload = ujson.loads(ans.object.object.message.payload)
-        db.shortcuts.update_admin_storage(
-            db.students.get_system_id_of_student(
-                ans.object.object.message.from_id,
-            ),
-            state_id=db.bot.get_id_of_state("main"),
-        )
-        student = models.Student.get_by_id(payload.get("student_id"))
-        email = student.email or "Не указан"
-        phone_number = student.phone_number or "Не указан"
-        contacts = "Контакты {0} {1}:\nВК: @id{2}\nEmail: {3}Телефон: {4}".format(
-            student.first_name,
-            student.second_name,
-            student.vk_id,
-            email,
-            phone_number,
-        )
-        await ans.answer(
-            contacts,
-            keyboard=kbs.main.main_menu(
-                db.students.get_system_id_of_student(ans.object.object.message.from_id),
-            ),
-        )
+    payload = ujson.loads(ans.object.object.message.payload)
+    db.shortcuts.update_admin_storage(
+        db.students.get_system_id_of_student(
+            ans.object.object.message.from_id,
+        ),
+        state_id=db.bot.get_id_of_state("main"),
+    )
+    student = models.Student.get_by_id(payload.get("student_id"))
+    email = student.email or "Не указан"
+    phone_number = student.phone_number or "Не указан"
+    contacts = "Контакты {0} {1}:\nВК: @id{2}\nEmail: {3}Телефон: {4}".format(
+        student.first_name,
+        student.second_name,
+        student.vk_id,
+        email,
+        phone_number,
+    )
+    await ans.answer(
+        contacts,
+        keyboard=kbs.main.main_menu(
+            db.students.get_system_id_of_student(ans.object.object.message.from_id),
+        ),
+    )
