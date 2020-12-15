1ec68e979f2b15d53beae2fcf17254e822f49e7c
Daniil <dadyarri@gmail.com>
2020-12-15 18:29:54 +0300

    chore(database): Refactor utils/finances.

--- jacob/database/utils/finances.py
+++ jacob/database/utils/finances.py
@@ -1,16 +1,16 @@
-import typing as t
+"""Функции для работы с блоком Финансы."""
+
+import typing
 from datetime import datetime
 
-from pony.orm import select
 from pony.orm import db_session
+from pony.orm import select
 
-from database.models import FinancialCategory
-from database.models import FinancialDonate
-from database.models import FinancialExpense
-from database.utils.students import get_active_students
+from jacob.database import models
+from jacob.database.utils.students import get_active_students
 
 
-def get_fin_categories(group_id: int) -> t.List[FinancialCategory]:
+def get_fin_categories(group_id: int) -> typing.List[models.FinancialCategory]:
     """
     Возвращает список категорий финансов группы.
 
@@ -20,14 +20,14 @@ def get_fin_categories(group_id: int) -> t.List[FinancialCategory]:
     Returns:
         List[FinancialCategory]: категории финансов
     """
-    return select(fc for fc in FinancialCategory if fc.group == group_id)[:]
+    return select(fc for fc in models.FinancialCategory if fc.group == group_id)[:]
 
 
 def add_or_edit_donate(
     category_id: int,
     student_id: int,
     summ: int,
-) -> FinancialDonate:
+) -> models.FinancialIncome:
     """
     Создает новый доход или редактирует существующий.
 
@@ -37,16 +37,16 @@ def add_or_edit_donate(
         summ: Сумма дохода
 
     Returns:
-        FinancialDonate: Объект дохода
+        FinancialIncome: Объект дохода
     """
-    donate = FinancialDonate.get(category=category_id, student=student_id)
+    donate = models.FinancialIncome.get(category=category_id, student=student_id)
     if donate is not None:
         with db_session:
             donate.summ = (donate.summ + summ,)
             donate.update_date = (datetime.now(),)
         return donate
     with db_session:
-        return FinancialDonate(
+        return models.FinancialIncome(
             category_id=category_id,
             student_id=student_id,
             summ=summ,
@@ -54,7 +54,7 @@ def add_or_edit_donate(
         )
 
 
-def get_debtors(category_id: int) -> t.List[int]:
+def get_debtors(category_id: int) -> typing.List[int]:
     """
     Ищет должников (не сдавших деньги на категорию вообще или не всю сумму).
 
@@ -64,18 +64,18 @@ def get_debtors(category_id: int) -> t.List[int]:
     Returns:
         List[int]: список идентификаторов
     """
-    category = FinancialCategory[category_id]
+    category = models.FinancialCategory[category_id]
     students = get_active_students(category.group)
     debtors = []
     for student in students:
-        donate = FinancialDonate.get(category=category_id, student=student.id)
+        donate = models.FinancialIncome.get(category=category_id, student=student.id)
         if donate is None or donate.summ < category.summ:
             debtors.append(student.id)
 
     return debtors
 
 
-def add_expense(category_id: int, summ: int) -> FinancialExpense:
+def add_expense(category_id: int, summ: int) -> models.FinancialExpense:
     """
     Создает новый расход.
 
@@ -87,13 +87,13 @@ def add_expense(category_id: int, summ: int) -> FinancialExpense:
         FinancialExpense: объект расхода
     """
     with db_session:
-        return FinancialExpense(
+        return models.FinancialExpense(
             category_id=category_id,
             summ=summ,
         )
 
 
-def calculate_donates_in_category(category_id: int) -> int:
+def calculate_incomes_in_category(category_id: int) -> int:
     """
     Вычисляет сумму собранных в категории денег.
 
@@ -103,7 +103,7 @@ def calculate_donates_in_category(category_id: int) -> int:
     Returns:
         int: Сумма сборов
     """
-    return sum(fd.summ for fd in FinancialDonate if fd.category == category_id)
+    return sum(fi.summ for fi in models.FinancialIncome if fi.category == category_id)
 
 
 def calculate_expenses_in_category(category_id: int) -> int:
@@ -116,12 +116,14 @@ def calculate_expenses_in_category(category_id: int) -> int:
     Returns:
         int: сумма расходов
     """
-    return sum(fd.summ for fd in FinancialExpense if fd.category == category_id)
+    return sum(fe.summ for fe in models.FinancialExpense if fe.category == category_id)
 
 
 def get_or_create_finances_category(
-    group_id: int, name: str, summ: int
-) -> FinancialCategory:
+    group_id: int,
+    name: str,
+    summ: int,
+) -> models.FinancialCategory:
     """
     Создаёт новую финансовую категорию, или возвращает уже существующую.
 
@@ -133,6 +135,7 @@ def get_or_create_finances_category(
     Returns:
         FinancialCategory: объект категории
     """
-    if FinancialCategory.exists(group_id=group_id, name=name, summ=summ):
-        return FinancialCategory.get(group_id=group_id, name=name, summ=summ)
-    return FinancialCategory(group_id=group_id, name=name, summ=summ)
+    if models.FinancialCategory.exists(group_id=group_id, name=name, summ=summ):
+        return models.FinancialCategory.get(group_id=group_id, name=name, summ=summ)
+    with db_session:
+        return models.FinancialCategory(group_id=group_id, name=name, summ=summ)
