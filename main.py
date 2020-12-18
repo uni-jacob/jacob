7ce6e2f2f70847756d2617ba796bcf69faa6019f
Daniil <dadyarri@gmail.com>
2020-12-18 13:53:01 +0300

    :bookmark: Update version to 2.15.2.

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ from services.exceptions import StudentNotFound
 from services.filters import PLFilter
 from services.logger.config import config
 
-__version__ = "2.15.1"
+__version__ = "2.15.2"
 
 from services.logger.handlers import InterceptHandler
 
