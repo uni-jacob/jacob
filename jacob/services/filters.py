0317869fb55b405ff7e898e9dae4a35f3bbe39a0
Daniil <dadyarri@gmail.com>
2021-01-16 13:38:32 +0300

    chore(services): Optimize imports.

--- jacob/services/filters.py
+++ jacob/services/filters.py
@@ -2,13 +2,9 @@ from json import JSONDecoder
 from typing import Dict
 
 import ujson
-from vkwave.bots import BaseEvent
-from vkwave.bots import BotType
-from vkwave.bots import PayloadFilter
+from vkwave.bots import BaseEvent, BotType, PayloadFilter
 from vkwave.bots.core import BaseFilter
-from vkwave.bots.core.dispatching.filters.base import FilterResult
-from vkwave.bots.core.dispatching.filters.base import NotFilter
-from vkwave.bots.core.dispatching.filters.builtin import get_payload
+from vkwave.bots.core.dispatching import filters
 from vkwave.types.bot_events import BotEventType
 
 from jacob.database import utils as db
@@ -23,9 +19,9 @@ class PLFilter(PayloadFilter):
     ):
         super().__init__(payload, json_loader)
 
-    async def check(self, event: BaseEvent) -> FilterResult:
-        if get_payload(event) is None:
-            return FilterResult(False)
+    async def check(self, event: BaseEvent) -> filters.base.FilterResult:
+        if filters.builtin.get_payload(event) is None:
+            return filters.base.FilterResult(False)
         if event.bot_type is BotType.USER:
             current_payload = self.json_loader(event.object.object.message_data.payload)
         elif event.object.type == BotEventType.MESSAGE_EVENT.value:
@@ -33,8 +29,8 @@ class PLFilter(PayloadFilter):
         else:
             current_payload = self.json_loader(event.object.object.message.payload)
         if current_payload is None:
-            return FilterResult(False)
-        return FilterResult(
+            return filters.base.FilterResult(False)
+        return filters.base.FilterResult(
             any(
                 self.payload.get(key, None) == val
                 for key, val in current_payload.items()
@@ -47,16 +43,16 @@ class StateFilter(BaseFilter):
         self.state = db.bot.get_id_of_state(state)
 
     def __invert__(self):
-        return NotFilter(self)
+        return filters.base.NotFilter(self)
 
-    async def check(self, event: BaseEvent) -> FilterResult:
+    async def check(self, event: BaseEvent) -> filters.base.FilterResult:
         try:
             admin_id = db.students.get_system_id_of_student(
                 event.object.object.message.from_id,
             )
         except StudentNotFound:
-            return FilterResult(False)
+            return filters.base.FilterResult(False)
         if not db.admin.is_user_admin(admin_id):
-            return FilterResult(False)
+            return filters.base.FilterResult(False)
         current_state = db.admin.get_admin_storage(admin_id).state_id.id
-        return FilterResult(current_state == self.state)
+        return filters.base.FilterResult(current_state == self.state)
