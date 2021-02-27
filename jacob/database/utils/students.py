d6dd45347382f04c0ff2e4e69ae4897049462a93
Daniil <dadyarri@gmail.com>
2021-02-27 18:54:26 +0300

    refactor(database): fix linter warnings

--- jacob/database/utils/students.py
+++ jacob/database/utils/students.py
@@ -1,4 +1,6 @@
-import typing as t
+"""Утилиты для работы со студентами в базе данных."""
+
+import typing
 
 from jacob.database.models import Student
 from jacob.database.utils import admin
@@ -16,18 +18,17 @@ def get_system_id_of_student(vk_id: int) -> int:
         int: идентификатор студента в системе
 
     Raises:
-        StudentNotFound: когда студент с указанным идентификатором ВК не найден в
-        системе
+        StudentNotFound: когда студент с указанным идентификатором ВК не найден в системе
     """
     student = Student.get(vk_id=vk_id)
     if student is not None:
         return student.id
     raise exceptions.StudentNotFound(
-        "Студента с id ВКонтакте {0} не существует в системе".format(vk_id)
+        "Студента с id ВКонтакте {0} не существует в системе".format(vk_id),
     )
 
 
-def get_active_students(group_id: int) -> t.List[Student]:
+def get_active_students(group_id: int) -> typing.List[Student]:
     """
     Возвращает список активных (не отчисленных студентов) конкретной группы.
 
@@ -47,7 +48,7 @@ def get_active_students(group_id: int) -> t.List[Student]:
     if students:
         return students
     raise exceptions.StudentNotFound(
-        "В группе {0} нет активных студентов".format(group_id)
+        "В группе {0} нет активных студентов".format(group_id),
     )
 
 
@@ -72,7 +73,7 @@ def get_unique_second_name_letters_in_a_group(group_id: int) -> list:
         return list(dict.fromkeys(snd_names))
 
 
-def get_list_of_students_by_letter(admin_id: int, letter: str) -> t.List[Student]:
+def get_list_of_students_by_letter(admin_id: int, letter: str) -> typing.List[Student]:
     """
     Возвращает объекты студентов активной группы, фамилии которых начинаются на letter.
 
