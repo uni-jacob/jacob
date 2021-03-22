79577c6a42e35f93a1e1bbfeb44e2ae868cc4038
Daniil <dadyarri@gmail.com>
2021-03-22 21:35:53 +0300

    fix(blueprints): fix bug with state system

--- jacob/blueprints/finances.py
+++ jacob/blueprints/finances.py
@@ -84,6 +84,10 @@ async def _register_category(ans: bots.SimpleBotEvent):
         admin_store = managers.AdminConfigManager(student_id)
         fin_store = managers.FinancialConfigManager(student_id)
         fin_store.update(financial_category=category.id)
+        state_storage = managers.StateStorageManager(student_id)
+        state_storage.update(
+            state=state_storage.get_id_of_state("fin_send_alert"),
+        )
         with orm.db_session:
             chat_id = admin_store.get_active_chat().vk_id
 
