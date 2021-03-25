45ab4e21ac29f8fb20283af2dbc6919cdcf1b7dd
Daniil <dadyarri@gmail.com>
2021-03-25 16:16:19 +0300

    feat(blueprints): add handler to display all lists of students in a group

--- jacob/blueprints/group.py
+++ jacob/blueprints/group.py
@@ -1,8 +1,10 @@
 """Блок Студенты."""
 
 from loguru import logger
+from pony import orm
 from vkwave import bots
 
+from jacob.database.utils import admin, students
 from jacob.services import filters
 from jacob.services import keyboard as kbs
 from jacob.services.logger import config as logger_config
@@ -20,4 +22,22 @@ async def _start_group(ans: bots.SimpleBotEvent):
     await ans.answer(
         "Меню группы",
         keyboard=kbs.group.group_menu(),
+    )
+
+
+@bots.simple_bot_message_handler(
+    group_router,
+    filters.PLFilter({"button": "lists"}),
+    bots.MessageFromConversationTypeFilter("from_pm"),
+)
+async def _start_lists(ans: bots.SimpleBotEvent):
+    admin_id = students.get_system_id_of_student(ans.from_id)
+    group_id = admin.get_active_group(admin_id).id
+
+    with orm.db_session:
+        kb = kbs.group.list_of_lists(group_id)
+
+    await ans.answer(
+        "Списки студентов группы",
+        keyboard=kb,
     )
