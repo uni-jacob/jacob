06eb50a4706b974924b0ef557c90311322db37fd
Daniil <dadyarri@gmail.com>
2021-03-25 16:15:14 +0300

    feat(keyboard): add keyboard to get all lists of students in a group

--- jacob/services/keyboard/group.py
+++ jacob/services/keyboard/group.py
@@ -1,4 +1,7 @@
+from pony import orm
 from vkwave.bots import Keyboard
+
+from jacob.database.utils import lists
 
 
 def group_menu():
@@ -7,10 +10,38 @@ def group_menu():
         "Студенты",
         payload={"button": "students"},
     )
+    kb.add_text_button("Списки", payload={"button": "lists"})
     kb.add_row()
     kb.add_text_button(
         text="◀️ Назад",
         payload={"button": "main_menu"},
+    )
+
+    return kb.get_keyboard()
+
+
+def list_of_lists(group_id: int):
+    kb = Keyboard()
+
+    with orm.db_session:
+        lists_of_group = lists.get_lists_of_group(group_id)
+
+    for lst in lists_of_group:
+        if len(kb.buttons[-1]) == 2:
+            kb.add_row()
+        kb.add_text_button(lst.name, payload={"button": "list", "list": lst.id})
+
+    if kb.buttons[-1]:
+        kb.add_row()
+
+    kb.add_text_button(
+        text="Создать список",
+        payload={"button": "create_list"},
+    )
+    kb.add_row()
+    kb.add_text_button(
+        text="◀️ Назад",
+        payload={"button": "group_mgmt"},
     )
 
     return kb.get_keyboard()
