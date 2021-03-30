60a6da374d2b8cf035d28a812abf9563e338ea56
Daniil <dadyarri@gmail.com>
2021-03-30 15:25:07 +0300

    feat(database): add util to find all public groups in university

--- jacob/database/utils/uni.py
+++ jacob/database/utils/uni.py
@@ -13,3 +13,12 @@ def get_all() -> orm.core.Query:
         orm.core.Query: Результаты запроса к БД университетов
     """
     return orm.select(uni for uni in models.AlmaMater)
+
+
+@orm.db_session
+def get_public_groups(university: int):
+    return orm.select(
+        group
+        for group in models.Group
+        if group.alma_mater.id == university and group.private is False
+    )
