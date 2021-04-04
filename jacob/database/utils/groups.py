d7c721ffecb5e7dc70be55abc6c9d1b7907ee927
Daniil <dadyarri@gmail.com>
2021-04-04 22:18:25 +0300

    feat(database): add util to get group's privacy

--- jacob/database/utils/groups.py
+++ jacob/database/utils/groups.py
@@ -15,3 +15,8 @@ def get_subgroups_in_group(group_id: int) -> list:
     return orm.select(st.subgroup for st in models.Student if st.group.id == group_id)[
         :
     ]
+
+
+@orm.db_session
+def get_privacy_of_group(group_id: int) -> bool:
+    return models.Group[group_id].private
