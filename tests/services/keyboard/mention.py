863fe112275945364d32545fed535ff88bebe2c6
Daniil <dadyarri@gmail.com>
2021-06-26 18:23:47 +0300

    test(keyboard): add blueprint to test mention's keyboards

---
+++ tests/services/keyboard/mention.py
@@ -0,0 +1,15 @@
+class TestMentionKeyboards:
+    def test_navigator(self):
+        pass
+
+    def test_skip_mention_message(self):
+        pass
+
+    def test_mention_prompt(self):
+        pass
+
+    def test_presets(self):
+        pass
+
+    def test_list_of_groups(self):
+        pass
