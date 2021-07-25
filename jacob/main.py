899560208cbcaae48af7b81d88d7dd5b56101a98
Daniil <dadyarri@gmail.com>
2021-07-25 17:01:40 +0300

    feat(blueprints): add greeting

--- jacob/main.py
+++ jacob/main.py
@@ -1,9 +1,27 @@
+import logging
 import os
 
 from vkbottle import Bot, load_blueprints_from_package
+from vkbottle.bot import Message
 
-bot = Bot(os.getenv("VK_TOKEN"))
+logging.basicConfig(level="DEBUG")
+
+bot = Bot(token=os.getenv("VK_TOKEN"))
+bot.labeler.vbml_ignore_case = True
+
 for bp in load_blueprints_from_package("blueprints"):
     bp.load(bot)
+
+
+@bot.on.message(text=["привет", "начать", "hello", "hi"])
+async def greeting(message: Message):
+    await message.answer("Привет!")
+
+    if ...:  # Собеседник не пользователь
+        await message.answer(
+            "Вы не являетесь пользователем. Создайте новую группу или введите код приглашения",
+            keyboard=...,
+        )
+
 
 bot.run_forever()
