96c0ac580602ac0d0c87ee663890c4cfcfd59ca8
Daniil <dadyarri@gmail.com>
2021-07-25 17:02:29 +0300

    feat(database): add User model

---
+++ jacob/database/models.py
@@ -0,0 +1,16 @@
+from tortoise import fields
+from tortoise.models import Model
+
+
+class User(Model):
+    """Зарегистрированный пользователь."""
+
+    id: int = fields.IntField(pk=True, description="ИД пользователя")
+    vk_id: int = fields.IntField(
+        null=False,
+        unique=True,
+        description="ИД ВК пользователя",
+    )
+    mode: int = fields.ForeignKeyField(
+        "BotMode", description="Текущий режим бота у пользователя"
+    )
