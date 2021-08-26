594f66433d90d54f3d4d2468eb947058c1703c69
Daniil <dadyarri@gmail.com>
2021-08-26 17:44:44 +0300

    feat(database): add function to get university by its id

--- jacob/database/utils/universities.py
+++ jacob/database/utils/universities.py
@@ -6,3 +6,16 @@ from jacob.database import models
 async def get_universities() -> list[models.University]:
     async with in_transaction():
         return await models.University.all()
+
+
+async def get_university_by_id(university_id: int) -> models.University:
+    """
+    Получает университет по его ИД.
+    Args:
+        university_id: ИД университета.
+
+    Returns:
+        University: Объект университета.
+    """
+    async with in_transaction():
+        return await models.University.get_or_none(id=university_id)
