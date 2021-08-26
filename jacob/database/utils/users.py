ce330cead921c2d0e13c3de9224f5c04bb4e703d
Daniil <dadyarri@gmail.com>
2021-08-26 15:04:04 +0300

    feat(database): add function to create users

--- jacob/database/utils/users.py
+++ jacob/database/utils/users.py
@@ -1,6 +1,7 @@
 from tortoise.transactions import in_transaction
 
 from jacob.database import models
+from jacob.database.utils.states import get_state_id_by_name
 
 
 async def is_user(vk_id: int) -> bool:
@@ -14,3 +15,22 @@ async def is_user(vk_id: int) -> bool:
     """
     async with in_transaction():
         return bool(await models.User.get_or_none(vk_id=vk_id))
+
+
+async def create_user(vk_id: int) -> models.User:
+    """
+    Создаёт нового пользователя и хранилище стейтов для него.
+
+    Args:
+        vk_id: ИД ВК нового пользователя
+
+    Returns:
+        models.User: Объект нового пользователя
+    """
+    async with in_transaction():
+        user = await models.User.create(vk_id=vk_id)
+        await models.StateStorage.create(
+            user_id=user.id,
+            state_id=get_state_id_by_name("main"),
+        )
+        return user
