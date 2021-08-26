7f00ec7f64562a053ded6d8d2c2442630ebda89e
Daniil <dadyarri@gmail.com>
2021-08-26 14:35:44 +0300

    feat(database): add basic functions to get states

---
+++ jacob/database/utils/states.py
@@ -0,0 +1,41 @@
+from typing import Optional
+
+from tortoise.transactions import in_transaction
+
+from jacob.database import models
+
+
+def get_state_id_by_name(state_name: str) -> Optional[int]:
+    """
+    Получает ИД стейта по его имени.
+    Args:
+        state_name: имя стейта
+
+    Returns:
+        Optional[int]: ИД стейта
+    """
+    async with in_transaction():
+        query = await models.State.get_or_none(description=state_name)
+
+        try:
+            return query.id
+        except AttributeError:
+            return None
+
+
+def get_state_name_by_id(state_id: int) -> Optional[str]:
+    """
+    Получает имя стейта по его ИД.
+    Args:
+        state_id: ИД стейта
+
+    Returns:
+        Optional[str]: имя стейта
+    """
+    async with in_transaction():
+        query = await models.State.get_or_none(id=state_id)
+
+        try:
+            return query.description
+        except AttributeError:
+            return None
