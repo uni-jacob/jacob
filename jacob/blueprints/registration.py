71ba39d286635070d55600d32c1e5562d10ecdb1
Daniil <dadyarri@gmail.com>
2021-08-26 20:01:52 +0300

    feat(blueprints): add empty handlers related to university registration

--- jacob/blueprints/registration.py
+++ jacob/blueprints/registration.py
@@ -2,6 +2,7 @@ from vkbottle.bot import Blueprint, Message
 
 from jacob.database.utils.users import set_state
 from jacob.services.rules import EventPayloadContainsRule
+from jacob.services import keyboards as kb
 
 bp = Blueprint("Group registration")
 bp.labeler.auto_rules = [EventPayloadContainsRule({"block": "registration"})]
@@ -14,4 +15,19 @@ async def init_registration(message: Message):
     await set_state(message.peer_id, "registration:select_university")
     await message.answer(
         "Выберите или создайте университет",
+        keyboard=await kb.list_of_universities(),
     )
+
+
+@bp.on.message(
+    EventPayloadContainsRule({"action": "university:select"}),
+)
+async def select_university(message: Message):
+    ...
+
+
+@bp.on.message(
+    EventPayloadContainsRule({"action": "university:create"}),
+)
+async def create_university(message: Message):
+    await message.answer("Введите название университета")
