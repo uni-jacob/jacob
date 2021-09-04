a59dbb0fb4326e366e015a8c596866910fc02540
Daniil <dadyarri@gmail.com>
2021-09-04 17:26:48 +0300

    feat(utils): add util to generate abbreviation from phrase

---
+++ jacob/services/common.py
@@ -0,0 +1,11 @@
+def generate_abbreviation(phrase: str) -> str:
+    """
+    Создаёт аббревиатуру из фразы.
+
+    Args:
+        phrase: Фраза
+
+    Returns:
+        str: Аббревиатура
+    """
+    return "".join([word[0].upper() for word in phrase.split(" ")])
