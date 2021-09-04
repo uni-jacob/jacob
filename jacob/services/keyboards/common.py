ed6c6de66be41ba4da6c508a09828bae1168bd0f
Daniil <dadyarri@gmail.com>
2021-09-04 17:26:10 +0300

    feat(keyboard): add universal keyboard with yes/no buttons

--- jacob/services/keyboards/common.py
+++ jacob/services/keyboards/common.py
@@ -7,3 +7,12 @@ def cancel() -> str:
     kb.add(Text("Отмена", {"action": "cancel"}), color=KeyboardButtonColor.NEGATIVE)
 
     return kb.get_json()
+
+
+def yes_no() -> str:
+    """Создаёт универсальную клавиатуру с кнопками "Да"/"Нет"."""
+    kb = Keyboard()
+    kb.add(Text("Да", {"action": "yes"}), color=KeyboardButtonColor.POSITIVE)
+    kb.add(Text("Нет", {"action": "no"}), color=KeyboardButtonColor.NEGATIVE)
+
+    return kb.get_json()
