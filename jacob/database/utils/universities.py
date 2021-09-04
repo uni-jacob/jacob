b2302133aff9ee5b0b4e95ce2f6dafb08fb84133
Daniil <dadyarri@gmail.com>
2021-09-04 17:27:19 +0300

    feat(database): add functions to create/update universities

--- jacob/database/utils/universities.py
+++ jacob/database/utils/universities.py
@@ -1,3 +1,5 @@
+import logging
+
 from tortoise.transactions import in_transaction
 
 from jacob.database import models
@@ -25,3 +27,33 @@ async def get_university_by_id(university_id: int) -> models.University:
     """
     async with in_transaction():
         return await models.University.get_or_none(id=university_id)
+
+
+async def create_new_university(university_name: str) -> models.University:
+    """
+    Создаёт новый университет.
+
+    Args:
+        university_name: Название университета
+
+    Returns:
+        University: Объект созданного университета
+    """
+    async with in_transaction():
+        return await models.University.create(name=university_name)
+
+
+async def update_university_abbreviation(university_id: int, new_abbreviation: str):
+    """
+    Изменяет аббревиатуру университета.
+
+    Args:
+        university_id: Идентификатор университета
+        new_abbreviation: Новая аббревиатура
+    """
+    async with in_transaction():
+        logging.debug(university_id)
+        logging.debug(new_abbreviation)
+        await models.University.filter(id=university_id).update(
+            abbreviation=new_abbreviation
+        )
