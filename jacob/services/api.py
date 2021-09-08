8e017bfa94e7ecc7256b5145477ae73fc35adeeb
Daniil <dadyarri@gmail.com>
2021-09-08 18:52:51 +0300

    feat(utils): add util to get payload of previous message

--- jacob/services/api.py
+++ jacob/services/api.py
@@ -1,3 +1,5 @@
+import json
+
 from vkbottle import EMPTY_KEYBOARD
 from vkbottle.bot import Message
 
@@ -5,3 +7,8 @@ from vkbottle.bot import Message
 async def send_empty_keyboard(message: Message):
     msg_id = await message.answer("...", keyboard=EMPTY_KEYBOARD)
     await message.ctx_api.messages.delete([msg_id], delete_for_all=True)
+
+
+async def get_previous_payload(message: Message, offset: int) -> dict:
+    query = await message.ctx_api.messages.get_by_id([message.id - offset])
+    return json.loads(query.items[0].payload)
