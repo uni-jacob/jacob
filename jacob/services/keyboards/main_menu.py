b024083746c3192c0a729163b336e4eb65d7d3bf
Daniil <dadyarri@gmail.com>
2021-09-08 22:13:51 +0300

    feat(keyboards): add basic keyboard of main menu

--- jacob/services/keyboards/main_menu.py
+++ jacob/services/keyboards/main_menu.py
@@ -6,3 +6,12 @@ def register_start() -> str:
     kb.add(Text("Регистрация", payload={"block": "registration", "action": "init"}))
 
     return kb.get_json()
+
+
+def main_menu(is_admin: bool) -> str:
+    kb = Keyboard()
+
+    if is_admin:
+        kb.add(Text("Расписание", payload={"block": "schedule", "action": "init"}))
+
+    return kb.get_json()
