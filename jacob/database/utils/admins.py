a23c425c5b0e1b6ab233c4d97d9a102c9ef0d9a0
Daniil <dadyarri@gmail.com>
2021-09-08 22:16:55 +0300

    feat(database): add util to check if user is admin

--- jacob/database/utils/admins.py
+++ jacob/database/utils/admins.py
@@ -3,6 +3,20 @@ from tortoise.transactions import in_transaction
 from jacob.database import models
 
 
+async def is_admin(user_id: int) -> bool:
+    """
+    Проверяет, является ли пользователь админом
+
+    Args:
+        user_id: ИД пользователя
+
+    Returns:
+        bool: Пользователь админ?
+    """
+    async with in_transaction():
+        return bool(await models.Admin.get_or_none(user_id=user_id))
+
+
 async def create_admin(user_id: int, group_id: int) -> models.Admin:
     """
     Создаёт нового администратора
