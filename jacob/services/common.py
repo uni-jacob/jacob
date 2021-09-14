f81cc91741fba47b641a4ffabc4fa8ad690ebe11
Daniil <dadyarri@gmail.com>
2021-09-15 02:10:42 +0300

    docs(utils): add docstring

--- jacob/services/common.py
+++ jacob/services/common.py
@@ -16,7 +16,16 @@ def generate_abbreviation(phrase: str) -> str:
     return "".join([word[0].upper() for word in phrase.split(" ")])
 
 
-def get_token():
+def get_token() -> str:
+    """
+    Получает токен от нужного сообщества в зависимости от выбранного окружения.
+
+    Raises:
+        UnknownEnvironmentType: Если указан некорректный тип окружения
+
+    Returns:
+        str: Токен VK Bot API
+    """
     environment = os.getenv("ENV").upper()
     if environment in ("PRODUCTION", "DEV"):
         return os.getenv(f"{environment}_VK_TOKEN")
