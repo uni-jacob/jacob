20e6763a7480a7e3b399434f1d8b64bf76188404
Daniil <dadyarri@gmail.com>
2021-09-15 01:59:59 +0300

    fix(database): fix name of the env var with database url

--- jacob/database/utils/init.py
+++ jacob/database/utils/init.py
@@ -5,7 +5,7 @@ from tortoise import Tortoise
 
 async def init_db_connection():
     await Tortoise.init(
-        db_url=os.getenv("LDATABASE_URL"),
+        db_url=os.getenv("DATABASE_URL"),
         modules={
             "models": ["jacob.database.models"],
         },
