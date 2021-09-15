067a934fff7f3a170ad864fe4324703636f320c7
Daniil <dadyarri@gmail.com>
2021-09-15 22:02:45 +0300

    feat(blueprints): add handler to list managed groups

---
+++ jacob/blueprints/main_menu.py
@@ -0,0 +1,20 @@
+from vkbottle.bot import Blueprint, Message
+
+from jacob.database.utils.groups import get_managed_groups
+from jacob.services.middleware import ChangeSentryUser
+from jacob.services.rules import EventPayloadContainsRule
+from jacob.services import keyboards as kb
+
+bp = Blueprint("UpperMenu")
+bp.labeler.message_view.register_middleware(ChangeSentryUser())
+
+
+@bp.on.message(
+    EventPayloadContainsRule({"block": "groups"}),
+    EventPayloadContainsRule({"action": "show"}),
+)
+async def show_managed_groups(message: Message):
+    managed_groups = await get_managed_groups(message.peer_id)
+    await message.answer(
+        "Выберите активные группы", keyboard=await kb.managed_groups(managed_groups)
+    )
