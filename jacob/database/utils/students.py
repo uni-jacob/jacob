0810347708ebf94725a0beccd8e94d394b619179
Daniil <dadyarri@gmail.com>
2021-09-18 00:29:50 +0300

    feat(database): add function to find exact student by parameters

--- jacob/database/utils/students.py
+++ jacob/database/utils/students.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Optional
 
 from tortoise.transactions import in_transaction
 
@@ -20,6 +21,19 @@ async def is_student(vk_id: int) -> bool:
         query = bool(await models.Student.get_or_none(user_id=user_id))
         logging.info(f"Пользователь {vk_id} {'' if query else 'не'} является студентом")
         return query
+
+
+async def get_student(**kwargs) -> Optional[models.Student]:
+    """Ищет одного студента в базе данных.
+
+    Args:
+        **kwargs: Параметры поиска
+
+    Returns:
+        Optional[Student]: Объект студента
+    """
+    async with in_transaction():
+        return await models.Student.get_or_none(**kwargs)
 
 
 async def create_student(
