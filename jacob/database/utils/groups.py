caa532d57201ccf65d2d1e8f7fa62f5228874d82
Daniil <dadyarri@gmail.com>
2021-09-18 19:49:33 +0300

    feat(database): add some utils to work with groups

--- jacob/database/utils/groups.py
+++ jacob/database/utils/groups.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Optional
 
 from tortoise.transactions import in_transaction
 
@@ -25,6 +26,31 @@ async def create_group(
     async with in_transaction():
         logging.info(f"Создание группы с параметрами {locals()}")
         return await models.Group.create(**locals())
+
+
+async def get_group(**kwargs) -> Optional[models.Group]:
+    """Ищет одну группу в базе данных.
+
+    Args:
+        **kwargs: Параметры поиска
+
+    Returns:
+        Optional[Group]: Объект группы
+    """
+    async with in_transaction():
+        return await models.Group.get_or_none(**kwargs)
+
+
+async def update_group(group_id: int, **kwargs):
+    """Обновляет группу в базе данных.
+
+    Args:
+        group_id: ИД группы
+        **kwargs: Новые значения полей
+
+    """
+    async with in_transaction():
+        return await models.Group.filter(id=group_id).update(**kwargs)
 
 
 async def get_managed_groups(vk_id: int) -> list[models.Admin]:
