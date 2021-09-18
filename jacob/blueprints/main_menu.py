5a41b5051da133d8b99857f9b9de7fe1a9d3856a
Daniil <dadyarri@gmail.com>
2021-09-18 19:51:17 +0300

    feat(blueprints): add toggling selecting of groups
    Resolves #124

--- jacob/blueprints/main_menu.py
+++ jacob/blueprints/main_menu.py
@@ -1,8 +1,11 @@
+import json
 import logging
 
 from vkbottle.bot import Blueprint, Message
 
+from jacob.database.utils.admins import toggle_group_selection
 from jacob.database.utils.groups import get_managed_groups
+from jacob.database.utils.users import get_user_id
 from jacob.services import keyboards as kb
 from jacob.services.middleware import ChangeSentryUser
 from jacob.services.rules import EventPayloadContainsRule
@@ -22,6 +25,16 @@ async def show_managed_groups(message: Message):
         "Выберите активные группы",
         keyboard=await kb.managed_groups(managed_groups),
     )
-    # TODO:
-    #   - Добавить хранилище активных групп
-    #   - Добавить переключение активности группы
+
+
+@bp.on.message(
+    EventPayloadContainsRule({"block": "groups"}),
+    EventPayloadContainsRule({"action": "select"}),
+)
+async def toggle_group(message: Message):
+    payload = json.loads(message.payload)
+    await toggle_group_selection(
+        payload.get("group_id"),
+        await get_user_id(message.peer_id),
+    )
+    await show_managed_groups(message)
