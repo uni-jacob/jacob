87600aa4119e408ad02b1cd0e71bac23fc258371
Daniil <dadyarri@gmail.com>
2021-09-19 22:39:04 +0300

    fix(keyboard): make keyboard synchronous

--- jacob/services/keyboards/groups.py
+++ jacob/services/keyboards/groups.py
@@ -3,7 +3,7 @@ from vkbottle import Keyboard, Text
 from jacob.database import models
 
 
-async def managed_groups(entries: list[models.Admin]) -> str:
+def managed_groups(entries: list[models.Admin]) -> str:
     """
     Генерирует клавиатуру со списком доступных для модерации групп
 
@@ -20,15 +20,13 @@ async def managed_groups(entries: list[models.Admin]) -> str:
         if len(kb.buttons) == 2:
             kb.row()
         selected = "✅ " if entry.is_active else ""
-        group = await entry.group
-        university = await group.university
         kb.add(
             Text(
-                f"{selected}{group.group_number}@{university.abbreviation}",
+                f"{selected}{entry.group.group_number}@{entry.group.university.abbreviation}",
                 {
                     "block": "groups",
                     "action": "select",
-                    "group_id": group.id,
+                    "group_id": entry.group.id,
                 },
             ),
         )
