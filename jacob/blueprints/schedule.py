d1352aa6edf4e75c23a19402ab8a46a8e0ef9df6
Daniil <dadyarri@gmail.com>
2021-09-20 22:43:15 +0300

    feat(blueprints): add handlers to select week and show available days

--- jacob/blueprints/schedule.py
+++ jacob/blueprints/schedule.py
@@ -1,6 +1,8 @@
 from vkbottle.bot import Blueprint, Message
 
 from jacob.database.utils.admins import is_admin
+from jacob.database.utils.schedule.days import get_days
+from jacob.database.utils.schedule.weeks import get_weeks
 from jacob.database.utils.users import get_user_id, set_state
 from jacob.services import keyboards as kb
 from jacob.services.middleware import ChangeSentryUser
@@ -20,3 +22,27 @@ async def open_schedule(message: Message):
     await set_state(message.peer_id, "schedule:main")
     is_admin_ = await is_admin(await get_user_id(message.peer_id))
     await message.answer("Блок Расписание", keyboard=kb.schedule_main(is_admin_))
+
+
+@bp.on.message(
+    EventPayloadContainsRule({"block": "schedule"}),
+    EventPayloadContainsRule(
+        {"action": "edit"},
+    ),
+)
+async def init_editing_schedule(message: Message):
+    await set_state(message.peer_id, "schedule:edit:select_week")
+    weeks = await get_weeks()
+    await message.answer("Выбор недели", keyboard=kb.weeks(weeks))
+
+
+@bp.on.message(
+    EventPayloadContainsRule({"block": "schedule"}),
+    EventPayloadContainsRule(
+        {"action": "select:week"},
+    ),
+)
+async def select_week(message: Message):
+    await set_state(message.peer_id, "schedule:edit:select_day")
+    days = await get_days()
+    await message.answer("Выберите день", keyboard=kb.days(days))
