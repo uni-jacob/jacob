0b6cca57e0869bc9fa91ddc138a07187ab92d3e0
Daniil <dadyarri@gmail.com>
2021-09-20 14:49:05 +0300

    feat(keyboards): add schedule's main keyboard

---
+++ jacob/services/keyboards/schedule.py
@@ -0,0 +1,10 @@
+from vkbottle import Keyboard, Text
+
+
+def schedule_main():
+    kb = Keyboard()
+    kb.add(Text("Редактировать", {"block": "schedule", "action": "edit"}))
+    kb.add(Text("Просмотреть", {"block": "schedule", "action": "view"}))
+    kb.row()
+    kb.add(Text("Назад", {"block": "main_menu"}))
+    return kb.get_json()
