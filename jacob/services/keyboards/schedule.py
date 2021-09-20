ae92cdb43f4412bc8ed7abab627097bd8206c23f
Daniil <dadyarri@gmail.com>
2021-09-20 21:50:11 +0300

    feat(keyboards): add keyboard to display available weeks

--- jacob/services/keyboards/schedule.py
+++ jacob/services/keyboards/schedule.py
@@ -1,5 +1,7 @@
 from vkbottle import Keyboard, Text
 
+from jacob.database import models
+
 
 def schedule_main(is_admin: bool) -> str:
     """
@@ -19,4 +21,31 @@ def schedule_main(is_admin: bool) -> str:
     kb.add(Text("Просмотреть", {"block": "schedule", "action": "view"}))
     kb.row()
     kb.add(Text("Назад", {"block": "main_menu"}))
+    return kb.get_json()
+
+
+def weeks(source: list[models.Week]) -> str:
+    """
+    Клавиатура со списком доступных недель.
+
+    Returns:
+        str: Клавиатура
+    """
+    kb = Keyboard()
+    for week in source:
+        if len(kb.buttons) == 2:
+            kb.row()
+        kb.add(
+            Text(
+                week.name,
+                {
+                    "block": "schedule",
+                    "action": "select:week",
+                    "week": week.id,
+                },
+            ),
+        )
+    kb.row()
+    kb.add(Text("Назад", {"block": "schedule", "action": "init"}))
+
     return kb.get_json()
