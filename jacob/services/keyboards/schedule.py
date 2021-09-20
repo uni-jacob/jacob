cb1f580804de1ad59aa3e53d526ee1e4508ec942
Daniil <dadyarri@gmail.com>
2021-09-20 21:48:09 +0300

    feat(keyboards): add displaying edit button only if user is admin

--- jacob/services/keyboards/schedule.py
+++ jacob/services/keyboards/schedule.py
@@ -1,15 +1,21 @@
 from vkbottle import Keyboard, Text
 
 
-def schedule_main() -> str:
+def schedule_main(is_admin: bool) -> str:
     """
     Главное меню расписания
+
+    Args:
+        is_admin: Пользователь администратор?
 
     Returns:
         str: Клавиатура с главным меню
     """
     kb = Keyboard()
-    kb.add(Text("Редактировать", {"block": "schedule", "action": "edit"}))
+
+    if is_admin:
+        kb.add(Text("Редактировать", {"block": "schedule", "action": "edit"}))
+
     kb.add(Text("Просмотреть", {"block": "schedule", "action": "view"}))
     kb.row()
     kb.add(Text("Назад", {"block": "main_menu"}))
