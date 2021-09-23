d0ab13e77ac4e4723d29d2d8ea792d18498ec2f9
Daniil <dadyarri@gmail.com>
2021-09-23 13:27:00 +0300

    feat(database): add util to create lesson time

--- jacob/database/utils/schedule/timetable.py
+++ jacob/database/utils/schedule/timetable.py
@@ -6,3 +6,10 @@ from jacob.database import models
 async def get_timetable(university_id: int) -> list[models.Timetable]:
     async with in_transaction():
         return await models.Timetable.filter(**locals())
+
+
+async def create_lesson_time(
+    university_id: int, start_time: str, end_time: str
+) -> models.Timetable:
+    async with in_transaction():
+        return await models.Timetable.create(**locals())
