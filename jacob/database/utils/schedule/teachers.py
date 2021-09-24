35f4c2142f1558636dab81f5d3d8a1330f138b89
Daniil <dadyarri@gmail.com>
2021-09-24 21:41:46 +0300

    feat(database): add util to create teacher

--- jacob/database/utils/schedule/teachers.py
+++ jacob/database/utils/schedule/teachers.py
@@ -15,3 +15,25 @@ async def get_teachers(university_id: int) -> list[models.Teacher]:
     """
     async with in_transaction():
         return await models.Teacher.filter(university_id=university_id)
+
+
+async def create_new_teacher(
+    university_id: int,
+    last_name: str,
+    first_name: str,
+    patronymic: str,
+) -> models.Teacher:
+    """
+    Создаёт нового преподавателя.
+
+    Args:
+        university_id: ИД университета
+        last_name: Фамилия преподавателя
+        first_name: Имя преподавателя
+        patronymic: Отчество преподавателя
+
+    Returns:
+        Teacher: Объект преподавателя
+    """
+    async with in_transaction():
+        return await models.Teacher.create(**locals())
