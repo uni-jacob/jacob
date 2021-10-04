90448975b4b8d1c8e43d6531dcb97acd426b3f39
Daniil <dadyarri@gmail.com>
2021-10-04 13:05:49 +0300

    style(database): translate comments into english

--- jacob/database/models/schedule.py
+++ jacob/database/models/schedule.py
@@ -5,112 +5,112 @@ from jacob.database.models import Group, Personality, University
 
 
 class DayOfWeek(Model):
-    id: int = fields.IntField(pk=True, description="ИД дня недели")
-    name: str = fields.CharField(max_length=11, description="Название дня недели")
-    abbreviation: str = fields.CharField(max_length=2, description="Сокращение")
+    id: int = fields.IntField(pk=True, description="Day of weeks ID")
+    name: str = fields.CharField(max_length=11, description="Day of weeks name")
+    abbreviation: str = fields.CharField(max_length=2, description="Abbreviation")
 
     class Meta:
         table = "daysofweek"
-        table_description = "Дни недели"
+        table_description = "Days of week"
 
 
 class Week(Model):
-    id: int = fields.IntField(pk=True, description="ИД недели")
-    name: str = fields.CharField(max_length=11, description="Название недели")
+    id: int = fields.IntField(pk=True, description="Weeks ID")
+    name: str = fields.CharField(max_length=11, description="Weeks name")
     abbreviation: str = fields.CharField(
         max_length=1,
-        description="Сокращение названия недели",
+        description="Week names abbreviation",
     )
 
     class Meta:
         table = "weeks"
-        table_description = "Учебные недели"
+        table_description = "Weeks"
 
 
 class LessonType(Model):
-    id: int = fields.IntField(pk=True, description="ИД типа занятия")
-    name: str = fields.CharField(max_length=30, description="Имя типа занятия")
-    abbreviation: str = fields.CharField(max_length=5, description="Сокращение")
+    id: int = fields.IntField(pk=True, description="Lesson types ID")
+    name: str = fields.CharField(max_length=30, description="Lesson types name")
+    abbreviation: str = fields.CharField(max_length=5, description="Abbreviation")
 
     class Meta:
         table = "lessontypes"
-        table_description = "Типы занятий"
+        table_description = "Lessons types"
 
 
 class Teacher(Personality):
     patronymic: str = fields.CharField(
         max_length=30,
-        description="Отчество преподавателя",
+        description="Teachers patronymic",
     )
     university: University = fields.ForeignKeyField(
         "models.University",
-        description="Университет",
+        description="University",
     )
 
     class Meta:
         table = "teachers"
-        table_description = "Преподаватели"
+        table_description = "Teachers"
 
 
 class Subject(Model):
-    id: int = fields.IntField(pk=True, description="ИД курса")
-    full_name: str = fields.CharField(max_length=255, description="Название курса")
+    id: int = fields.IntField(pk=True, description="Subjects ID")
+    full_name: str = fields.CharField(max_length=255, description="Subjects name")
     abbreviation: str = fields.CharField(
         max_length=15,
-        description="Аббревиатура названия",
+        description="Subjects abbreviation",
     )
     group: Group = fields.ForeignKeyField(
         "models.Group",
-        description="Группа",
+        description="Groups",
     )
 
     class Meta:
         table = "subjects"
-        table_description = "Учебные курсы"
+        table_description = "Subjects"
 
 
 class Classroom(Model):
-    id: int = fields.IntField(pk=True, description="ИД аудитории")
+    id: int = fields.IntField(pk=True, description="Classrooms ID")
     university: University = fields.ForeignKeyField(
         "models.University",
-        description="Университет",
+        description="University",
     )
-    building: int = fields.IntField(description="Учебный корпус")
-    class_name: str = fields.CharField(max_length=10, description="Номер аудитории")
+    building: int = fields.IntField(description="Buildings number")
+    class_name: str = fields.CharField(max_length=10, description="Classrooms number")
 
     class Meta:
         table = "classrooms"
-        table_description = "Аудитории"
+        table_description = "Classrooms"
 
 
 class Timetable(Model):
-    id: int = fields.IntField(pk=True, description="ИД аудитории")
+    id: int = fields.IntField(pk=True, description="Time ID")
     university: University = fields.ForeignKeyField(
         "models.University",
-        description="Университет",
+        description="University",
     )
     start_time: str = fields.CharField(max_length=5)
     end_time: str = fields.CharField(max_length=5)
 
 
 class Lesson(Model):
-    id: int = fields.IntField(pk=True, description="ИД курса")
+    id: int = fields.IntField(pk=True, description="Lessons ID")
     lesson_type: LessonType = fields.ForeignKeyField(
         "models.LessonType",
-        description="ИД типа занятия",
+        description="Lesson types ID",
     )
-    week: Week = fields.ForeignKeyField("models.Week", description="ИД недели")
-    time: Timetable = fields.ForeignKeyField("models.Timetable", description="ИД пары")
-    subject: Subject = fields.ForeignKeyField("models.Subject", description="ИД курса")
+    week: Week = fields.ForeignKeyField("models.Week", description="Weeks ID")
+    time: Timetable = fields.ForeignKeyField("models.Timetable", description="Time ID")
+    subject: Subject = fields.ForeignKeyField("models.Subject", description="Course ID")
     teacher: Teacher = fields.ForeignKeyField(
         "models.Teacher",
-        description="ИД преподавателя",
+        description="Teachers ID",
     )
     classroom: Classroom = fields.ForeignKeyField(
         "models.Classroom",
-        description="ИД аудитории",
+        description="Classrooms ID",
     )
 
     class Meta:
         table = "lessons"
-        table_description = "Пары"
+        table_description = "Lessons"
