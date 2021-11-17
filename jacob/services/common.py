b771056f2f8bb50fa8a60be69973367d53d34e77
Daniil <dadyarri@gmail.com>
2021-11-17 20:27:15 +0300

    refactor: move some functions to config module, according to naming conventions

--- jacob/services/common.py
+++ jacob/services/common.py
@@ -1,9 +1,6 @@
 import logging
-import os
 
 from vkbottle.dispatch.rules.bot import VBMLRule
-
-from jacob.services.exceptions import UnknownEnvironmentType
 
 
 def generate_abbreviation(phrase: str) -> str:
@@ -19,41 +16,6 @@ def generate_abbreviation(phrase: str) -> str:
     abbr = "".join([word[0].upper() for word in phrase.split(" ")])
     logging.info(f"Аббревиатура {phrase} - {abbr}")
     return abbr
-
-
-def get_token() -> str:
-    """
-    Получает токен от нужного сообщества в зависимости от выбранного окружения.
-
-    Raises:
-        UnknownEnvironmentType: Если указан некорректный тип окружения
-
-    Returns:
-        str: Токен VK Bot API
-    """
-    environment = os.getenv("ENV").upper()
-    var_name = f"{environment}_VK_TOKEN"
-    token = os.getenv(var_name)
-    if token is None:
-        raise UnknownEnvironmentType(f"{environment} не определён")
-
-    logging.info(f"Выбран токен {var_name}")
-    return token
-
-
-def get_database_url() -> str:
-    """
-    Получает URL базы данных в зависимости от того, запущен ли проект с помощью pytest.
-
-    Returns:
-        str: URL базы данных
-    """
-    if "PYTEST_CURRENT_TEST" in os.environ:
-        logging.debug("Выбрана тестовая БД")
-        return os.getenv("TEST_DATABASE_URL")
-
-    logging.debug("Выбрана основная БД")
-    return os.getenv("DATABASE_URL")
 
 
 def vbml_rule(bot):
