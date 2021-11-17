5b1788d6ac0c60067bedc0246b1c722c2d9edb35
Daniil <dadyarri@gmail.com>
2021-11-17 21:01:54 +0300

    feat(database): add functions to create & update subjects

--- jacob/database/utils/schedule/subjects.py
+++ jacob/database/utils/schedule/subjects.py
@@ -6,3 +6,15 @@ from jacob.database import models
 async def get_subjects(group_id: int) -> list[models.Subject]:
     async with in_transaction():
         return await models.Subject.filter(group_id=group_id)
+
+
+async def create_subject(group: models.Group, full_name: str) -> models.Subject:
+    async with in_transaction():
+        return await models.Subject.create(group=group, full_name=full_name)
+
+
+async def update_subject(subject_id: int, **kwargs) -> models.Subject:
+    async with in_transaction():
+        query = await models.Subject(id=subject_id).update_from_dict(kwargs)
+        await query.save()
+        return await models.Subject.get(id=subject_id)
