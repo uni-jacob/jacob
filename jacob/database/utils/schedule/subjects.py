eddd6133789cf829a52b7ec511487c8b85a343ca
Daniil <dadyarri@gmail.com>
2021-11-17 21:42:55 +0300

    feat(database): add function to find subject

--- jacob/database/utils/schedule/subjects.py
+++ jacob/database/utils/schedule/subjects.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from tortoise.transactions import in_transaction
 
 from jacob.database import models
@@ -6,6 +8,11 @@ from jacob.database import models
 async def get_subjects(group_id: int) -> list[models.Subject]:
     async with in_transaction():
         return await models.Subject.filter(group_id=group_id)
+
+
+async def find_subject(**kwargs) -> Optional[models.Subject]:
+    async with in_transaction():
+        return await models.Subject.get_or_none(**kwargs)
 
 
 async def create_subject(group: models.Group, full_name: str) -> models.Subject:
