91262a936bc7eef19887648c55566952ad233ba2
Daniil <dadyarri@gmail.com>
2021-11-26 20:24:18 +0300

    refactor(schedule): remove schedule's menu

--- jacob/blueprints/schedule/menu.py
+++ jacob/blueprints/schedule/menu.py
@@ -1,8 +1,7 @@
 from vkbottle.bot import Blueprint, Message
 
-from jacob.database.utils.admins import is_admin
 from jacob.database.utils.schedule.weeks import get_weeks
-from jacob.database.utils.users import get_user_id, set_state
+from jacob.database.utils.users import set_state
 from jacob.services import keyboards
 from jacob.services.common import vbml_rule
 from jacob.services.middleware import ChangeSentryUser
@@ -21,17 +20,5 @@ vbml_rule = vbml_rule(bp)
 )
 async def open_schedule(message: Message):
     await set_state(message.peer_id, "schedule:main")
-    is_admin_ = await is_admin(await get_user_id(message.peer_id))
-    await message.answer("Блок Расписание", keyboard=keyboards.schedule_main(is_admin_))
-
-
-@bp.on.message(
-    EventPayloadContainsRule({"block": "schedule"}),
-    EventPayloadContainsRule(
-        {"action": "edit"},
-    ),
-)
-async def init_editing_schedule(message: Message):
-    await set_state(message.peer_id, "schedule:edit:select_week")
     weeks = await get_weeks()
     await message.answer("Выбор недели", keyboard=keyboards.weeks(weeks))
