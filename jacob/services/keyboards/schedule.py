2f47d3863a7442b09fa2aa77966cd5e9ccf67555
Daniil <dadyarri@gmail.com>
2021-11-26 20:58:46 +0300

    refactor(keyboards): remove unused keyboard

--- jacob/services/keyboards/schedule.py
+++ jacob/services/keyboards/schedule.py
@@ -3,27 +3,6 @@ from vkbottle import Keyboard, Text
 from jacob.database import models
 
 
-def schedule_main(is_admin: bool) -> str:
-    """
-    Главное меню расписания
-
-    Args:
-        is_admin: Пользователь администратор?
-
-    Returns:
-        str: Клавиатура с главным меню
-    """
-    kb = Keyboard()
-
-    if is_admin:
-        kb.add(Text("Редактировать", {"block": "schedule", "action": "edit"}))
-
-    kb.add(Text("Просмотреть", {"block": "schedule", "action": "view"}))
-    kb.row()
-    kb.add(Text("Назад", {"block": "main_menu"}))
-    return kb.get_json()
-
-
 def weeks(source: list[models.Week]) -> str:
     """
     Клавиатура со списком доступных недель.
